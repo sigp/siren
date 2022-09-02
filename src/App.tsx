7f73ec34bdc0dea71308f1a1cb04e636d1202065
Mavrik <mrricki.m.usmc@gmail.com>
2022-09-02 04:53:06 +0100

    feat: setup react app with typescript, storybook and tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="h-screen w-screen bg-slate-700 flex items-center justify-center text-white">
+      LightHouse Ui
+    </div>
+  );
+}
+
+export default App;
