cd2a867369eb9889eb97e7eab548974439b07e77
Ricki Moore <mrricki.m.usmc@gmail.com>
2022-09-02 16:07:34 +0100

    feat: progress circle (#5)

---
+++ src/ProgressCircle/ProgressCircle.stories.tsx
@@ -0,0 +1,44 @@
+import ProgressCircle, {ProgressCircleProps} from "./ProgressCircle";
+import {Story} from "@storybook/react";
+
+
+export default {
+    title: 'Progress Circle',
+    component: ProgressCircle
+}
+
+const Template: Story<ProgressCircleProps> = (props) => (
+    <ProgressCircle {...props}/>
+)
+
+export const Base = Template.bind({});
+Base.args = {};
+
+export const PartialPrimary = Template.bind({});
+PartialPrimary.args = {
+    percent: 25
+};
+
+export const FullPrimary = Template.bind({});
+FullPrimary.args = {
+    percent: 100
+};
+
+export const PartialSecondary = Template.bind({});
+PartialSecondary.args = {
+    percent: 25,
+    color: 'secondary'
+};
+
+export const FullSecondary = Template.bind({});
+FullSecondary.args = {
+    percent: 100,
+    color: 'secondary'
+};
+
+export const CounterClock = Template.bind({});
+CounterClock.args = {
+    percent: 25,
+    color: 'secondary',
+    direction: 'counter'
+};
