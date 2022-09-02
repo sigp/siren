7fd2e1d68dd16640d7a431681e0c932fe0bb0b72
Ricki Moore <mrricki.m.usmc@gmail.com>
2022-09-02 10:50:04 +0100

    fix: app renderer (#3)

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './global.css';
 import App from './App';
+import {StrictMode} from "react";
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const container = document.getElementById('root');
+const root = createRoot(container!);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <StrictMode>
+        <App />
+    </StrictMode>
 );
