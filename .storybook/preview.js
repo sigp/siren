d3f04e5b1f7f35b9856c2ddf28f65d66df87651b
Mavrik <mrricki.m.usmc@gmail.com>
2022-09-05 18:45:36 +0100

    feat: added darkmode to storybook and bootstrap icons

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 import '../src/global.css';
+import { themes } from '@storybook/theming'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -8,4 +9,15 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  darkMode: {
+    dark: {
+      ...themes.dark,          // copy existing values
+      appContentBg: '#1E1E1E', // override main story view frame
+      barBg: '#202020'         // override top toolbar
+    }
+  }
 }
+
+export const globalTypes = {
+  darkMode: true,
+};
