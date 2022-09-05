6e8d56cbe38b89ce1312d44873eab64036a4565c
Mavrik <mrricki.m.usmc@gmail.com>
2022-09-05 18:48:31 +0100

    feat: added footbar component and story

---
+++ src/components/FootBar/FootBar.stories.tsx
@@ -0,0 +1,15 @@
+import FootBar from "./FootBar";
+import {Story} from "@storybook/react";
+
+
+export default {
+    key: 'FootBar',
+    component: FootBar
+}
+
+const Template:Story = () => (
+    <FootBar/>
+);
+
+export const Base = Template.bind({});
+Base.args = {};
