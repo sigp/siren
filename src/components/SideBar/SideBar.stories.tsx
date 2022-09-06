cc1e440441f078abb2f471e2b2627b6f9ee35e0c
Mavrik <mrricki.m.usmc@gmail.com>
2022-09-06 18:03:19 +0100

    feat: added sidebar component

---
+++ src/components/SideBar/SideBar.stories.tsx
@@ -0,0 +1,16 @@
+import SideBar from "./SideBar";
+import {Story} from "@storybook/react";
+
+export default {
+    key: 'SideBar',
+    component: SideBar
+}
+
+const Template:Story = () => (
+    <div className="w-screen h-screen overflow-hidden">
+        <SideBar/>
+    </div>
+);
+
+export const Base = Template.bind({});
+Base.args = {};
