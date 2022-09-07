7afc787df9ac26537f682146175477a1c8e3ff9e
Mavrik <mrricki.m.usmc@gmail.com>
2022-09-07 17:41:57 +0100

    feat: added sync metric component and story

---
+++ src/components/SyncMetric/SyncMetric.stories.tsx
@@ -0,0 +1,29 @@
+import SyncMetric, {SyncMetricProps} from "./SyncMetric";
+import {Story} from "@storybook/react";
+
+export default {
+    key: 'SyncMetric',
+    component: SyncMetric
+}
+
+const Template:Story<SyncMetricProps> = (props) => (
+    <SyncMetric {...props} />
+)
+
+export const Base = Template.bind({});
+Base.args = {
+    title: 'ETHEREUM MAINNET',
+    color: 'secondary',
+    id: 'ethMain',
+    total: 212245,
+    amount: 150435,
+};
+
+export const CounterClock = Template.bind({});
+CounterClock.args = {
+    title: 'BEACON CHAIN',
+    direction: 'counter',
+    id: 'beaconChain',
+    total: 212245,
+    amount: 120435,
+};
