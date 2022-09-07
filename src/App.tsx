4afbc86a6e5ab4783178420deb91a0671117ed1b
Mavrik <mrricki.m.usmc@gmail.com>
2022-09-07 12:53:46 +0100

    feat: added dark mode classname with recoil state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import {useRecoilValue} from "recoil";
+import {uiMode} from "./recoil/atoms";
+import {UiMode} from "./constants/enums";
+import Dashboard from "./views/DashBoard/Dashboard";
 
 function App() {
+    const mode = useRecoilValue(uiMode);
   return (
-    <div className="h-screen w-screen bg-slate-700 flex items-center justify-center text-white">
-      LightHouse Ui
+    <div className={`${mode === UiMode.DARK ? 'dark' : ''} h-screen w-screen overflow-hidden flex`}>
+        <Dashboard/>
     </div>
   );
 }
