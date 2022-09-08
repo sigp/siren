9e2f1f5c1acc3ee63613a94a5781efd32397e395
Mavrik <mrricki.m.usmc@gmail.com>
2022-09-08 20:47:00 +0100

    feat: added new typography color and img assets

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -26,7 +26,7 @@ type TagsSupported = typeof tagsSupported[number];
 
 export type TypographyFamily = 'font-openSauce' | 'font-roboto' | 'font-archivo';
 
-export type TypographyColor = 'text-dark900' | 'text-dark500' | 'text-dark300' | 'text-dark400' |'text-primary' | 'text-white'
+export type TypographyColor = 'text-dark900' | 'text-dark500' | 'text-dark300' | 'text-dark400' |'text-primary' | 'text-white' | 'text-transparent'
 
 export type TypographyType = 'text-caption2' | 'text-caption1' | 'text-body' | 'text-subtitle2' | 'text-subtitle1' | 'text-h3' | 'text-h2' | 'text-h1' | 'text-title' | 'text-tiny';
 
