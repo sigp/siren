43f8e8cbf30b0bfff607f4a4cb1372dcb93e4dce
Mavrik <mrricki.m.usmc@gmail.com>
2022-09-16 18:02:02 +0100

    feat: created tab option component for form

---
+++ src/components/TabOption/TabOption.tsx
@@ -0,0 +1,23 @@
+import Typography from "../Typography/Typography";
+import {FC} from "react";
+
+export interface TabOptionProps {
+    toolTip?: string,
+    isActive?: boolean,
+    text: string,
+    onClick?: () => void
+}
+
+const TabOption:FC<TabOptionProps> = ({text, toolTip, isActive, onClick}) => {
+  return (
+      <div onClick={onClick} className="space-y-3 cursor-pointer group">
+          <div className="flex items-center space-x-2">
+              <Typography family="font-archivo" color={isActive ? 'text-white' : 'text-dark200'} type="text-caption1" className="group-hover:text-white uppercase">{text}</Typography>
+              {toolTip && <i className="bi-question-circle-fill text-caption1 text-white"/>}
+          </div>
+          {isActive && <hr className="bg-white w-full h-0.5"/>}
+      </div>
+  )
+}
+
+export default TabOption;
