1ca0f7bffd5828a0c570edee0fb90ef13dbf502a
Mavrik <mrricki.m.usmc@gmail.com>
2022-09-21 12:58:40 +0100

    fix: adjusted input size with diff media queries

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -16,13 +16,13 @@ const Input:FC<InputProps> = ({label, error, tooltip, type, ...props}) => {
       <div className="space-y-4 w-full">
           {label && (
               <div className="flex items-center space-x-2">
-                  <Typography isBold family="font-archivo" color="text-dark500" className="uppercase" type="text-caption1">{label}</Typography>
-                  {tooltip && <i className="bi-question-circle-fill text-caption1 text-dark500" />}
+                  <Typography isBold family="font-archivo" color="text-dark500" className="uppercase md:text-caption1" type="text-caption2">{label}</Typography>
+                  {tooltip && <i className="bi-question-circle-fill text-caption2 md:text-caption1 text-dark500" />}
               </div>
           )}
           <div className="relative w-full">
-              <input {...props} type={inputType} className={`${isPasswordType ? 'pr-5' : ''} w-full bg-transparent placeholder:text-dark500 font-light font-openSauce outline-none text-white border-b border-white text-subtitle1`}/>
-              {isPasswordType && <i onClick={togglePassword} className={`cursor-pointer ${inputType === 'password' ? 'bi-eye-slash-fill' : 'bi-eye-fill'} text-dark500 absolute right-0 top-1/2 -translate-y-1/2`}/>}
+              <input {...props} type={inputType} className={`${isPasswordType ? 'pr-5' : ''} pb-2 w-full bg-transparent placeholder:text-dark500 font-light font-openSauce outline-none text-white border-b border-white text-body md:text-subtitle1`}/>
+              {isPasswordType && <i onClick={togglePassword} className={`cursor-pointer text-caption1 md:text-body ${inputType === 'password' ? 'bi-eye-slash-fill' : 'bi-eye-fill'} text-dark500 absolute right-0 top-1/2 -translate-y-1/2`}/>}
           </div>
       </div>
   )
--- src/components/ProtocolInput/ProtocolInput.tsx
+++ src/components/ProtocolInput/ProtocolInput.tsx
@@ -46,11 +46,11 @@ const ProtocolInput:FC<ProtocolInputProps> = ({id, control, setValue, getValues,
 
  return (
      <div className="flex items-end justify-between border-b border-white w-full pb-4">
-         <div className="flex space-x-4">
+         <div className="flex flex-wrap space-x-1 md:space-x-4">
              <div className="space-y-4">
                  <div className="flex">
                      <Typography isBold color="text-dark500" className="w-16 flex-grow-0 uppercase" type="text-tiny">Protocol (http/https)</Typography>
-                     <i className="bi-question-circle-fill text-caption1 text-dark500" />
+                     <i className="bi-question-circle-fill text-caption2 md:text-caption1 text-dark500" />
                  </div>
                  <Controller
                      name={`${defaultType}.protocol`}
@@ -62,16 +62,16 @@ const ProtocolInput:FC<ProtocolInputProps> = ({id, control, setValue, getValues,
              </div>
              <div className="space-y-4">
                  <div className="flex space-x-2 items-center">
-                     <Typography isBold color="text-dark500" className="flex-grow-0 uppercase" type="text-caption1">{`${type === 'validatorClient' ? 'VC' : 'Beacon'} ADDRESS`}</Typography>
-                     <i className="bi-question-circle-fill text-caption1  text-dark500" />
+                     <Typography isBold color="text-dark500" className="flex-grow-0 uppercase md:text-caption1" type="text-caption2">{`${type === 'validatorClient' ? 'VC' : 'Beacon'} ADDRESS`}</Typography>
+                     <i className="bi-question-circle-fill text-caption2 md:text-caption1  text-dark500" />
                  </div>
-                 <div className="flex w-44 items-center">
-                     <Typography color="text-dark500" className="flex-grow-0 lowercase" type="text-subtitle2">{`${protocol}://`}</Typography>
+                 <div className="relative flex w-28 md:w-44 items-center">
+                     <Typography color="text-dark500" className="flex-grow-0 lowercase md:text-subtitle2">{`${protocol}://`}</Typography>
                      <Controller
                          name={`${type || 'beaconNode'}.address`}
                          control={control}
                          render={({field: {ref: _ref, value}}) => (
-                             <input className="text-subtitle2 flex-1 bg-transparent text-white outline-none" type="text" value={value} onChange={onChangeAddress} />
+                             <input className="text-body w-full md:text-subtitle2 flex-1 bg-transparent text-white outline-none" type="text" value={value} onChange={onChangeAddress} />
                          )}
                      />
                  </div>
@@ -81,27 +81,27 @@ const ProtocolInput:FC<ProtocolInputProps> = ({id, control, setValue, getValues,
                      <>
                          <div className="space-y-4">
                              <div className="flex space-x-2 items-center">
-                                 <Typography isBold color="text-dark500" className="flex-grow-0 uppercase" type="text-caption1">BN Port</Typography>
-                                 <i className="bi-question-circle-fill text-caption1  text-dark500" />
+                                 <Typography isBold color="text-dark500" className="flex-grow-0 uppercase md:text-caption1" type="text-caption2">BN Port</Typography>
+                                 <i className="bi-question-circle-fill text-caption2 md:text-caption1  text-dark500" />
                              </div>
                              <Controller
                                  name="beaconNode.port"
                                  control={control}
                                  render={({field: {ref: _ref, ...props}}) => (
-                                     <input className="text-subtitle2 w-20 bg-transparent text-white outline-none appearance-none" type="number" {...props} />
+                                     <input className="text-body md:text-subtitle2 w-20 bg-transparent text-white outline-none appearance-none" type="number" {...props} />
                                  )}
                              />
                          </div>
                          <div className="space-y-4">
                              <div className="flex space-x-2 items-center">
-                                 <Typography isBold color="text-dark500" className="flex-grow-0 uppercase" type="text-caption1">VC Port</Typography>
-                                 <i className="bi-question-circle-fill text-caption1  text-dark500" />
+                                 <Typography isBold color="text-dark500" className="flex-grow-0 uppercase md:text-caption1" type="text-caption2">VC Port</Typography>
+                                 <i className="bi-question-circle-fill text-caption2 md:text-caption1  text-dark500" />
                              </div>
                              <Controller
                                  name="validatorClient.port"
                                  control={control}
                                  render={({field: {ref: _ref, ...props}}) => (
-                                     <input className="text-subtitle2 w-20 bg-transparent text-white outline-none appearance-none" type="number" {...props} />
+                                     <input className="text-body md:text-subtitle2 w-20 bg-transparent text-white outline-none appearance-none" type="number" {...props} />
                                  )}
                              />
                          </div>
@@ -109,21 +109,21 @@ const ProtocolInput:FC<ProtocolInputProps> = ({id, control, setValue, getValues,
                  ) : (
                      <div className="space-y-4">
                          <div className="flex space-x-2 items-center">
-                             <Typography isBold color="text-dark500" className="flex-grow-0 uppercase" type="text-caption1">{`${type === 'beaconNode' ? 'BN' : 'VC'} Port`}</Typography>
-                             <i className="bi-question-circle-fill text-caption1  text-dark500" />
+                             <Typography isBold color="text-dark500" className="flex-grow-0 uppercase md:text-caption1" type="text-caption2">{`${type === 'beaconNode' ? 'BN' : 'VC'} Port`}</Typography>
+                             <i className="bi-question-circle-fill text-caption2 md:text-caption1  text-dark500" />
                          </div>
                          <Controller
                              name={`${defaultType}.port`}
                              control={control}
                              render={({field: {ref: _ref, value}}) => (
-                                 <input className="text-subtitle2 w-20 bg-transparent text-white outline-none appearance-none" type="number" value={value} onChange={onChangePort} />
+                                 <input className="text-body md:text-subtitle2 w-20 bg-transparent text-white outline-none appearance-none" type="number" value={value} onChange={onChangePort} />
                              )}
                          />
                      </div>
                  )
              }
          </div>
-         <i className={`bi-check-circle-fill text-subtitle2 ${isValid ? 'text-success' : 'text-dark500'}`} />
+         <i className={`bi-check-circle-fill text-body md:text-subtitle2 ${isValid ? 'text-success' : 'text-dark500'}`} />
      </div>
  )
 }
