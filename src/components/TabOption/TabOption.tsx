278c4cb2bb86dba28600ba1de77ced4ef5e0e013
Mavrik <mrricki.m.usmc@gmail.com>
2022-09-21 12:59:31 +0100

    feat: added svg animation and other input fields

--- src/components/TabOption/TabOption.tsx
+++ src/components/TabOption/TabOption.tsx
@@ -12,8 +12,8 @@ const TabOption:FC<TabOptionProps> = ({text, toolTip, isActive, onClick}) => {
   return (
       <div onClick={onClick} className="space-y-3 cursor-pointer group">
           <div className="flex items-center space-x-2">
-              <Typography family="font-archivo" color={isActive ? 'text-white' : 'text-dark200'} type="text-caption1" className="group-hover:text-white uppercase">{text}</Typography>
-              {toolTip && <i className="bi-question-circle-fill text-caption1 text-white"/>}
+              <Typography family="font-archivo" color={isActive ? 'text-white' : 'text-dark200'} type="text-caption2" className="md:text-caption1 group-hover:text-white uppercase">{text}</Typography>
+              {toolTip && <i className="bi-question-circle-fill text-caption2 md:text-caption1 text-white"/>}
           </div>
           {isActive && <hr className="bg-white w-full h-0.5"/>}
       </div>
