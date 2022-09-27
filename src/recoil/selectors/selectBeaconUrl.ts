efd9708ddd241c430b411f2a41c969d3776f8f16
Mavrik <mrricki.m.usmc@gmail.com>
2022-09-27 20:25:57 +0100

    chore: created base url selectors from validator and beacon atoms

---
+++ src/recoil/selectors/selectBeaconUrl.ts
@@ -0,0 +1,14 @@
+import { selector } from 'recoil'
+import { beaconNodeEndpoint } from '../atoms'
+
+export const selectBeaconUrl = selector({
+  key: 'beaconUrl',
+  get: ({ get }) => {
+    const beaconNode = get(beaconNodeEndpoint)
+    if (!beaconNode) return undefined
+
+    const { protocol, address, port } = beaconNode
+
+    return `${protocol}://${address}:${port}/eth`
+  },
+})
