7d13644f47fb9b6c9578af408cc6c8218f3f72f9
Mavrik <mrricki.m.usmc@gmail.com>
2022-10-03 20:24:00 +0100

    feat: added bg images to sync cards

--- src/components/SyncCard/SyncCard.tsx
+++ src/components/SyncCard/SyncCard.tsx
@@ -1,6 +1,8 @@
 import Typography from '../Typography/Typography'
 import ProgressCircle from '../ProgressCircle/ProgressCircle'
 import { FC } from 'react'
+import {ReactComponent as NotAvailable} from '../../assets/images/notAvalilable.svg'
+import {ReactComponent as LightEth} from '../../assets/images/lightEth.svg'
 
 export interface SyncCardProps {
   title: string
@@ -8,9 +10,10 @@ export interface SyncCardProps {
   timeRemaining: string
   progress: number
   status: string
+  type?: 'beacon' | 'geth'
 }
 
-const SyncCard: FC<SyncCardProps> = ({ title, subTitle, timeRemaining, status, progress }) => {
+const SyncCard: FC<SyncCardProps> = ({ title, subTitle, timeRemaining, status, progress, type }) => {
   return (
     <div className='flex-1 space-y-6 border border-dark100 p-4'>
       <div className='w-full flex justify-between'>
@@ -20,8 +23,15 @@ const SyncCard: FC<SyncCardProps> = ({ title, subTitle, timeRemaining, status, p
         </div>
         <Typography>{timeRemaining}</Typography>
       </div>
-      <div className='w-full flex items-center justify-center'>
+      <div className='relative w-full flex items-center justify-center'>
         <ProgressCircle percent={progress} size='xl' id='geth' />
+        <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2">
+          {type ? (
+            <LightEth className="h-24 text-dark100"/>
+          ) : (
+            <NotAvailable className="h-16 text-dark100" />
+          )}
+        </div>
       </div>
       <Typography type='text-subtitle2'>{status}</Typography>
     </div>
