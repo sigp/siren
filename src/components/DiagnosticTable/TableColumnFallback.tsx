199201a3785a1b31a12ea7349c8bdf8d0602f058
Mavrik <mrricki.m.usmc@gmail.com>
2022-10-04 15:48:21 +0100

    feat: created column fallback component for suspense

---
+++ src/components/DiagnosticTable/TableColumnFallback.tsx
@@ -0,0 +1,16 @@
+import Spinner from '../Spinner/Spinner';
+import { FC } from 'react';
+
+export interface TableColumnFallbackProps {
+  size?: 'sm' | 'lg'
+}
+
+const TableColumnFallback:FC<TableColumnFallbackProps> = ({size = 'sm'}) => {
+  return (
+    <div className={`h-full w-full flex flex-col items-center justify-center ${size === 'lg' ? 'xl:min-w-316' : ''} border border-dark200 dark:border-dark500`}>
+      <Spinner/>
+    </div>
+  )
+}
+
+export default TableColumnFallback;
