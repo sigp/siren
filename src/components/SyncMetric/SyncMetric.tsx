a59e3a52ccdd1f697c990fbd265c5db4807ec76f
Mavrik <mrricki.m.usmc@gmail.com>
2022-10-06 05:48:55 +0100

    fix: moved sync metric fallback to component file

--- src/components/SyncMetric/SyncMetric.tsx
+++ src/components/SyncMetric/SyncMetric.tsx
@@ -2,6 +2,18 @@ import { FC } from 'react'
 import Typography from '../Typography/Typography'
 import ProgressCircle, { ProgressCircleProps } from '../ProgressCircle/ProgressCircle'
 import { formatLocalCurrency } from '../../utilities/formatLocalCurrency'
+import Spinner from '../Spinner/Spinner';
+
+
+export const SyncMetricFallback = () => {
+  return (
+    <div
+      className="flex w-40 h-14 max-h-full bg-white flex items-center justify-center dark:bg-dark750 border border-borderLight dark:border-dark900"
+    >
+      <Spinner size="h-6 w-6"/>
+    </div>
+  )
+}
 
 export interface SyncMetricProps extends ProgressCircleProps {
   total: number
