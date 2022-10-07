3fe98a09f839e756a175c3102bc5ae74c373aba5
Mavrik <mrricki.m.usmc@gmail.com>
2022-10-07 11:43:11 +0100

    feat: added util to format eth addresses

---
+++ src/utilities/formatEthAddress.ts
@@ -0,0 +1,9 @@
+export default function formatEthAddress(
+  address: string,
+  padStart = 6,
+  padEnd = 4,
+) {
+  return address
+    ? `${address.slice(0, padStart)}...${address.slice(-padEnd)}`
+    : '';
+}
