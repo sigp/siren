1af1c4df72452f1dd66f9c3700468e493d7192ad
Mavrik <mrricki.m.usmc@gmail.com>
2022-10-17 11:04:53 +0100

    fix: use sync info selector instead of hook

--- src/hooks/useValidatorEarnings.ts
+++ src/hooks/useValidatorEarnings.ts
@@ -1,17 +1,17 @@
 import { useRecoilValue } from 'recoil'
 import { selectValidatorInfos } from '../recoil/selectors/selectValidatorInfos'
-import { useMemo } from 'react'
-import useBeaconSyncInfo from './useBeaconSyncInfo'
+import { useMemo } from 'react';
 import { fetchValidatorStatuses } from '../api/beacon'
 import { selectBeaconUrl } from '../recoil/selectors/selectBeaconUrl'
 import { BeaconValidatorResult } from '../types/validator'
 import { formatUnits } from 'ethers/lib/utils'
 import { initialEthDeposit } from '../constants/constants'
+import { selectBeaconSyncInfo } from '../recoil/selectors/selectBeaconSyncInfo';
 
 const useValidatorEarnings = () => {
   const validators = useRecoilValue(selectValidatorInfos)
   const baseBeaconUrl = useRecoilValue(selectBeaconUrl)
-  const { headSlot } = useBeaconSyncInfo()
+  const { headSlot } = useRecoilValue(selectBeaconSyncInfo)
 
   const validatorKeys = useMemo(() => {
     return validators.map((validator) => validator.pubKey).join(',')
