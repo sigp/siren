361c1d064af092ea14bd3f3062924ba9f30da2c7
Mavrik <mrricki.m.usmc@gmail.com>
2022-10-19 21:38:27 +0100

    feat: added hook to handle intervals

---
+++ src/hooks/usePollingInterval.ts
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from 'react';
+
+const usePollingInterval = (callback: () => void, delay: number) => {
+  const savedCallback = useRef<() => void>();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current?.();
+    }
+    if (delay !== null) {
+      const id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
+export default usePollingInterval;
