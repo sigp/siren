8a63fb7a9ea0b2b8d62e3900350fd3218864b015
Mavrik <mrricki.m.usmc@gmail.com>
2022-10-20 14:53:06 +0100

    feat: added skip and onClear callback options

--- src/hooks/usePollingInterval.ts
+++ src/hooks/usePollingInterval.ts
@@ -1,6 +1,11 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
-const usePollingInterval = (callback: () => void, delay: number) => {
+const usePollingInterval = (
+  callback: () => void,
+  delay: number,
+  options?: { isSkip: boolean; onClearInterval?: () => void },
+): { intervalId: NodeJS.Timer | undefined } => {
+  const [intervalId, setId] = useState<NodeJS.Timer>()
   const savedCallback = useRef<() => void>()
 
   useEffect(() => {
@@ -8,14 +13,25 @@ const usePollingInterval = (callback: () => void, delay: number) => {
   }, [callback])
 
   useEffect(() => {
+    if (options?.isSkip) return
+
     function tick() {
       savedCallback.current?.()
     }
     if (delay !== null) {
       const id = setInterval(tick, delay)
-      return () => clearInterval(id)
+      setId(id)
+      return () => {
+        options?.onClearInterval?.()
+        clearInterval(id)
+        setId(undefined)
+      }
     }
   }, [delay])
+
+  return {
+    intervalId,
+  }
 }
 
 export default usePollingInterval
