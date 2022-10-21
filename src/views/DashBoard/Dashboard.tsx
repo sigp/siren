4eee0bfe96441476d2419313dfd6c5733421e97b
Mavrik <mrricki.m.usmc@gmail.com>
2022-10-21 12:58:32 +0100

    fix: prevent sync until dashboard finish mount

--- src/views/DashBoard/Dashboard.tsx
+++ src/views/DashBoard/Dashboard.tsx
@@ -1,20 +1,29 @@
-import SideBar from '../../components/SideBar/SideBar'
-import FootBar from '../../components/FootBar/FootBar'
-import React from 'react'
-import MainContent from './Content/MainContent'
-import { useRecoilValue } from 'recoil'
-import { dashView } from '../../recoil/atoms'
-import { ContentView } from '../../constants/enums'
-import Logs from './Content/Logs'
-import Settings from './Content/Settings'
-import Validators from './Content/Validators'
-import Grafana from './Content/Grafana'
-import TopBar from '../../components/TopBar/TopBar'
-import useBeaconSyncPolling from '../../hooks/useBeaconSyncPolling'
+import SideBar from '../../components/SideBar/SideBar';
+import FootBar from '../../components/FootBar/FootBar';
+import React, { useEffect, useState } from 'react';
+import MainContent from './Content/MainContent';
+import { useRecoilValue } from 'recoil';
+import { dashView } from '../../recoil/atoms';
+import { ContentView } from '../../constants/enums';
+import Logs from './Content/Logs';
+import Settings from './Content/Settings';
+import Validators from './Content/Validators';
+import Grafana from './Content/Grafana';
+import TopBar from '../../components/TopBar/TopBar';
+import useBeaconSyncPolling from '../../hooks/useBeaconSyncPolling';
+
+const Sync = () => {
+  useBeaconSyncPolling()
+  return null
+}
 
 const Dashboard = () => {
   const content = useRecoilValue(dashView)
-  useBeaconSyncPolling()
+  const [isReadySync, setSync] = useState(false)
+
+  useEffect(() => {
+    setSync(true)
+  }, [])
 
   const renderContent = () => {
     switch (content) {
@@ -32,6 +41,7 @@ const Dashboard = () => {
   }
   return (
     <>
+      {isReadySync && <Sync/>}
       <SideBar />
       <div className='flex flex-1 flex-col bg-white dark:bg-darkPrimary items-center justify-center'>
         <TopBar />
