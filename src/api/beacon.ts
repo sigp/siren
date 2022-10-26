0ddcbbc8a48a65de5e40da66f56e6626de168b13
Mavrik <mrricki.m.usmc@gmail.com>
2022-10-26 16:41:52 +0100

    feat: added api for fetching beacon version

--- src/api/beacon.ts
+++ src/api/beacon.ts
@@ -4,6 +4,9 @@ import { Endpoint } from '../forms/ConfigConnectionForm'
 export const fetchSyncStatus = async ({ protocol, address, port }: Endpoint) =>
   await axios.get(`${protocol}://${address}:${port}/eth/v1/node/syncing`)
 
+export const fetchBeaconVersion = async ({protocol, address, port}: Endpoint) =>
+  await axios.get(`${protocol}://${address}:${port}/eth/v1/node/version`)
+
 export const fetchValidatorStatuses = async (
   baseBeaconUrl: string,
   validatorKeys: string,
