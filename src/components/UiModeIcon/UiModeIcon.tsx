39ae79b375ef4d5254d103ec295e2c86418f6149
Mavrik <mrricki.m.usmc@gmail.com>
2022-10-28 17:21:26 +0100

    feat: added settings view content

---
+++ src/components/UiModeIcon/UiModeIcon.tsx
@@ -0,0 +1,19 @@
+import { UiMode } from '../../constants/enums';
+import { ReactComponent as ToggleSun } from '../../assets/images/sun.svg';
+import { ReactComponent as ToggleMoon } from '../../assets/images/moon.svg';
+import { FC } from 'react';
+
+export interface UiModeIconProps {
+  mode?: UiMode
+  onClick?: () => void
+}
+
+const UiModeIcon:FC<UiModeIconProps> = ({mode, onClick}) => {
+  return (
+    <div onClick={onClick} className='w-4 h-4 cursor-pointer text-dark400'>
+      {mode === UiMode.LIGHT ? <ToggleSun /> : <ToggleMoon />}
+    </div>
+  )
+}
+
+export default UiModeIcon;
