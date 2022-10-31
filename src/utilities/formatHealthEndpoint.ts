587af953ad6ca41e93af6048d923d1cf240d063a
Mavrik <mrricki.m.usmc@gmail.com>
2022-10-31 20:20:06 +0000

    feat: created util to create health endpoint string

---
+++ src/utilities/formatHealthEndpoint.ts
@@ -0,0 +1,9 @@
+import { Endpoint } from '../types';
+
+
+const formatHealthEndpoint = (endpoint?: Endpoint) => {
+  const { protocol, address, port } = endpoint || {};
+  return endpoint ? `${protocol}://${address}:${port}/lighthouse/ui/health` : undefined
+}
+
+export default formatHealthEndpoint;
