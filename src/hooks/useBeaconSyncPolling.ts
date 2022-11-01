f89bd7862919f73397d0284136d95d712872d3dc
Mavrik <mrricki.m.usmc@gmail.com>
2022-11-01 15:35:01 +0000

    chore: refactored beacon sync hook

--- src/hooks/useBeaconSyncPolling.ts
+++ src/hooks/useBeaconSyncPolling.ts
@@ -1,46 +1,33 @@
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
-import { beaconNodeEndpoint, beaconSyncInfo, beaconSyncInterval } from '../recoil/atoms'
-import { fetchSyncStatus } from '../api/beacon'
+import { useRecoilValue, useSetRecoilState } from 'recoil'
+import { beaconNodeEndpoint, beaconSyncInfo, beaconSyncInterval } from '../recoil/atoms';
 import { useEffect } from 'react'
-import usePollingInterval from './usePollingInterval'
 import { secondsInSlot } from '../constants/constants'
+import usePollApi from './usePollApi';
 
-const useBeaconSyncPolling = () => {
+const useBeaconSyncPolling = (time = secondsInSlot * 1000) => {
   const beaconNode = useRecoilValue(beaconNodeEndpoint)
+  const { protocol, address, port } = beaconNode || {};
+  const url = beaconNode ? `${protocol}://${address}:${port}/eth/v1/node/syncing` : undefined
   const setBeaconSyncInfo = useSetRecoilState(beaconSyncInfo)
 
-  const [interval, setIntervalId] = useRecoilState(beaconSyncInterval)
-  const onClearInterval = () => setIntervalId(undefined)
-  const isSkip = Boolean(interval)
-
-  const fetchBeaconInfo = async () => {
-    try {
-      const { data } = await fetchSyncStatus(beaconNode)
-
-      setBeaconSyncInfo({
-        ...data.data,
-        head_slot: Number(data.data.head_slot),
-        sync_distance: Number(data.data.sync_distance),
-      })
-    } catch (e) {
-      console.error(e)
-    }
-  }
-
-  const { intervalId } = usePollingInterval(fetchBeaconInfo, secondsInSlot * 1000, {
-    isSkip,
-    onClearInterval,
-  })
+  const { response } = usePollApi({
+    time,
+    isReady: !!url,
+    intervalState: beaconSyncInterval,
+    url
+  });
 
   useEffect(() => {
-    if (intervalId) {
-      setIntervalId(intervalId)
-    }
-  }, [intervalId])
+    if(!response) return
 
-  useEffect(() => {
-    void fetchBeaconInfo()
-  }, [])
+    const data = response.data.data;
+
+    setBeaconSyncInfo({
+      ...data,
+      head_slot: Number(data.head_slot),
+      sync_distance: Number(data.sync_distance),
+    })
+  }, [response])
 }
 
 export default useBeaconSyncPolling
