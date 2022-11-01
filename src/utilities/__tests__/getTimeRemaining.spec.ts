ed40b7cc9cc0c7f928d799da6c9829c29c43131b
Mavrik <mrricki.m.usmc@gmail.com>
2022-11-01 08:30:55 +0000

    feat: added unit tests for time remaining and shortand utils

---
+++ src/utilities/__tests__/getTimeRemaining.spec.ts
@@ -0,0 +1,13 @@
+import getTimeRemaining from '../getTimeRemaining';
+
+
+describe('Get time remaining util', () => {
+  it('should return correct times', () => {
+    expect(getTimeRemaining(91810)).toStrictEqual({
+      days: 1,
+      hours: 1,
+      minutes: 30,
+      seconds: 10,
+    })
+  })
+})
