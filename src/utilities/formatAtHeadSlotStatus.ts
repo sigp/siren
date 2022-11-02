a38e3f2c58b206b09739404bae8936c2ead735d7
Mavrik <mrricki.m.usmc@gmail.com>
2022-11-02 10:40:57 +0000

    fix: fix the status color for at-headslot

--- src/utilities/formatAtHeadSlotStatus.ts
+++ src/utilities/formatAtHeadSlotStatus.ts
@@ -6,9 +6,9 @@ const formatAtHeadSlotStatus = (status?: number): StatusType | 'bg-dark100' => {
   switch (true) {
     case status >= -1:
       return 'bg-success'
-    case status < -2 && status > -5:
+    case status <= -2 && status > -5:
       return 'bg-warning'
-    case status < -5:
+    case status <= -5:
       return 'bg-error'
     default:
       return 'bg-dark100'
