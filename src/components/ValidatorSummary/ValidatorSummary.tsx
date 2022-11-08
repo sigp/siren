6ca71a3847eac05d7c80124de2f9daf28a71378f
Mavrik <mrricki.m.usmc@gmail.com>
2022-11-08 19:09:25 +0000

    fix: extracted income summary into component

--- src/components/ValidatorSummary/ValidatorSummary.tsx
+++ src/components/ValidatorSummary/ValidatorSummary.tsx
@@ -1,33 +1,18 @@
 import { useRecoilValue } from 'recoil';
 import { selectValidatorInfos } from '../../recoil/selectors/selectValidatorInfos';
 import Typography from '../Typography/Typography';
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import Status from '../Status/Status';
-import useValidatorEarnings from '../../hooks/useValidatorEarnings';
-import { slotsInDay } from '../../constants/constants';
 import { useTranslation } from 'react-i18next';
+import ValidatorIncomeSummary from '../ValidatorIncomeSummary/ValidatorIncomeSummary';
 
 const ValidatorSummary = () => {
   const { t } = useTranslation()
   const validators = useRecoilValue(selectValidatorInfos)
-  const { fetchHistory } = useValidatorEarnings();
-  const [income, setIncome] = useState('0')
 
   const totalBalance = useMemo(() => {
     return validators.map(validator => validator.balance).reduce((a,b) => a + b, 0)
   }, [validators])
-
-  const fetchIncome = async (time: number) => {
-    const earnings = await fetchHistory(time);
-
-    if(earnings) {
-      setIncome(earnings.toFixed(3))
-    }
-  }
-
-  useEffect(() => {
-    void fetchIncome(slotsInDay)
-  }, [])
 
   return (
     <div className="w-full max-w-850 flex flex-col lg:space-x-4 shadow lg:flex-row lg:divide-x divide-y lg:divide-y-0 dark:divide-dark600 dark:border dark:border-dark600">
@@ -45,16 +30,7 @@ const ValidatorSummary = () => {
           <Typography isBold type="text-caption1">{totalBalance.toFixed(3)} ETH</Typography>
         </div>
       </div>
-      <div className="p-3 space-y-2">
-        <Typography type="text-caption2" isBold isUpperCase>{t('validatorManagement.summary.totalIncome')}</Typography>
-        <div className="flex items-center space-x-6">
-          <Typography color="text-dark300" type="text-caption1">{t('validatorManagement.day')}</Typography>
-          <div className="flex items-center space-x-1">
-            <i className={`text-sm ${Number(income) > 0 ? 'bi-chevron-up text-success' : 'bi-chevron-down text-error'}`}/>
-            <Typography isBold type="text-caption1">{income} ETH</Typography>
-          </div>
-        </div>
-      </div>
+      <ValidatorIncomeSummary validators={validators} className="p-3 space-y-2 w-42"/>
       <div className="p-3 space-y-2">
         <Typography type="text-caption2" isBold isUpperCase>{t('validatorManagement.summary.avgEffectiveness')}</Typography>
         <div className="flex space-x-8">
