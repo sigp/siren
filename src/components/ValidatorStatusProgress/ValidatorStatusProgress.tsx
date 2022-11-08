fecc7123e2c01c27d7139dbd69dd0b1b7eb83728
Mavrik <mrricki.m.usmc@gmail.com>
2022-11-08 19:08:32 +0000

    feat: added validator progress bar components

---
+++ src/components/ValidatorStatusProgress/ValidatorStatusProgress.tsx
@@ -0,0 +1,44 @@
+import { ValidatorStatus } from '../../types/validator';
+import { FC } from 'react';
+import Typography from '../Typography/Typography';
+import ProgressIcon from './ProgressIcon';
+import getValidatorStatusStage from '../../utilities/getValidatorStatusStage';
+import ProgressBar from './ProgressBar';
+
+export interface ValidatorStatusProgressProps {
+  status: ValidatorStatus
+}
+
+const ValidatorStatusProgress:FC<ValidatorStatusProgressProps> = ({status}) => {
+  const stage = getValidatorStatusStage(status)
+  const isDeposit = stage >= 1
+  const isPending = stage >= 2
+  const isActive = stage >= 3
+  const isExit = stage >= 4
+
+  return (
+    <div className="w-full flex items-center">
+      <div className="flex flex-col items-center">
+        <Typography type="text-caption2" color="text-dark300"><i className="bi-1-circle text-tiny"/> Deposit</Typography>
+        <ProgressIcon isActive={isDeposit} className="text-subtitle2"/>
+      </div>
+      <ProgressBar isActive={isDeposit}/>
+      <div className="flex flex-col items-center">
+        <Typography type="text-caption2" color="text-dark300"><i className="bi-2-circle text-tiny"/> Pending</Typography>
+        <ProgressIcon isActive={isPending} className="text-subtitle2"/>
+      </div>
+      <ProgressBar isActive={isPending}/>
+      <div className="flex flex-col items-center">
+        <Typography type="text-caption2" color="text-dark300"><i className="bi-3-circle text-tiny"/> Active</Typography>
+        <ProgressIcon isActive={isActive} className="text-subtitle2"/>
+      </div>
+      <ProgressBar isActive={isExit}/>
+      <div className="flex flex-col items-center ml-1">
+        <Typography type="text-caption2" color="text-dark300"><i className="bi-4-circle text-tiny"/> Exit</Typography>
+        <ProgressIcon isActive={isExit} className="text-subtitle2"/>
+      </div>
+    </div>
+  )
+}
+
+export default ValidatorStatusProgress;
