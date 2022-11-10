dacac8167cdd7a75eb80520937e1f7ce8a2e4489
Mavrik <mrricki.m.usmc@gmail.com>
2022-11-10 19:42:11 +0000

    fix: added tranlations and fixed card action props

--- src/components/ValidatorModal/ValidatorCardAction.tsx
+++ src/components/ValidatorModal/ValidatorCardAction.tsx
@@ -10,7 +10,7 @@ export interface ValidatorCardActionProps {
 }
 
 
-const ValidatorCardAction:FC<ValidatorCardActionProps> = ({icon, title, onClick, className}) => {
+const ValidatorCardAction:FC<ValidatorCardActionProps> = ({icon, title, onClick, className = 'w-32 @425:w-36 sm:w-96 mr-3 mb-2 lg:flex-1'}) => {
   return (
     <div onClick={onClick} className={`border p-4 space-y-2 ${className}`}>
       <ValidatorActionIcon icon={icon} color="text-primary"/>
--- src/components/ValidatorModal/views/ValidatorDetails.tsx
+++ src/components/ValidatorModal/views/ValidatorDetails.tsx
@@ -36,13 +36,13 @@ const ValidatorDetails = () => {
   return validator ? (
     <div className="w-full">
       <div className="w-full flex flex-col lg:flex-row">
-        <div className="w-full lg:w-80 h-60 border-r px-8  py-4 relative">
+        <div className="w-full lg:w-80 h-60 border-r-style100 px-8 lg:px-6 py-4 relative">
           <img
             alt='waves'
             src={Waves}
             className='z-10 w-full h-full absolute left-0 top-0 opacity-10'
           />
-          <div className="w-full flex flex-col justify-between space-y-3 h-full z-20 relative">
+          <div className="w-full flex flex-col justify-between space-y-3 lg:space-y-0 h-full z-20 relative">
             <div className="w-full flex justify-between">
               <div className="bg-gradient-to-br from-primaryBright via-primary to-secondary h-36 w-36 rounded-full"/>
               <div className="text-right flex-1 flex flex-col justify-between">
@@ -53,7 +53,7 @@ const ValidatorDetails = () => {
                 <div className="space-y-2">
                   <div>
                     <Typography color="text-dark300">—</Typography>
-                    <Typography type="text-caption2" color="text-dark300" isBold isUpperCase>Validator <br/> Balance</Typography>
+                    <Typography type="text-caption2" color="text-dark300" isBold isUpperCase>{t('validatorManagement.details.validatorBalance')}</Typography>
                   </div>
                   <Typography isBold>{balance?.toFixed(4)}</Typography>
                 </div>
@@ -65,7 +65,7 @@ const ValidatorDetails = () => {
                 <Typography type="text-caption1" isBold >{`${pubKey?.substring(0, 12)}...`}</Typography>
               </div>
               <div className="flex-1 space-y-2">
-                <Typography type="text-caption2" color="text-dark300" isUpperCase>index</Typography>
+                <Typography type="text-caption2" color="text-dark300" isUpperCase>{t('validatorManagement.details.index')}</Typography>
                 <Typography type="text-caption1" isBold >{index}</Typography>
               </div>
             </div>
@@ -73,10 +73,10 @@ const ValidatorDetails = () => {
         </div>
         <div className="flex-1 flex py-4 justify-center items-center">
           <div className="w-11/12 space-y-2">
-            <div className="w-full flex flex-col lg:flex-row shadow">
-              <div className="p-3 flex lg:flex-col lg:border-r space-y-4">
+            <div className="w-full flex flex-col border-style100 lg:flex-row lg:shadow">
+              <div className="p-3 flex lg:flex-col lg:border-r-style100 justify-between">
                 <div className="space-y-2">
-                  <Typography isBold type="text-caption1">Balance</Typography>
+                  <Typography isBold type="text-caption1">{t('validatorManagement.details.balance')}</Typography>
                   <div className="flex w-full space-x-4 justify-between">
                     <Typography type="text-caption1" color="text-dark300">USD</Typography>
                     <Typography type="text-caption1" isBold>${formatLocalCurrency(usdBalance)}</Typography>
@@ -90,11 +90,11 @@ const ValidatorDetails = () => {
                   </div>
                 </div>
               </div>
-              <div className="space-y-4 border-t lg:border-t-0 lg:border-r flex flex-col justify-between">
+              <div className="space-y-4 border-t-style100 lg:border-t-0 lg:border-r-style100 flex flex-col justify-between">
                 <ValidatorIncomeSummary validators={[validator]} className="pt-4 px-2 space-y-2 w-42"/>
                 <div className="w-full flex border-t lg:border-none">
                   <div className="space-y-3 flex-1 pt-4 lg:pt-0 border-r px-2 pb-2">
-                    <Typography isBold type="text-caption2">Slots Behind</Typography>
+                    <Typography isBold type="text-caption2">{t('validatorManagement.details.slotsBehind')}</Typography>
                     <Typography className="text-right">0</Typography>
                   </div>
                   <div className="space-y-3 flex-1 pt-4 lg:pt-0 px-2 pb-2">
@@ -107,21 +107,21 @@ const ValidatorDetails = () => {
                 </div>
               </div>
               <a target="_blank" rel="noreferrer" href={`${BeaconChaValidatorUrl}/${index}`}>
-                <div className="cursor-pointer bg-dark25 flex-1 space-y-3 p-3">
+                <div className="cursor-pointer dark:bg-dark700 bg-dark25 flex-1 space-y-3 p-3">
                   <Typography type="text-caption1" isBold className="uppercase">Beacon <br/> Cha.in</Typography>
                   <Typography color="text-dark400" fontWeight="font-light" type="text-caption2">
-                    Explore more on beaconchai.in dashboard
+                    {t('validatorManagement.details.exploreBeaconChai')}
                   </Typography>
                   <div className="w-full flex items-center justify-between">
-                    <Typography fontWeight="font-light" className="uppercase">Explorer</Typography>
+                    <Typography fontWeight="font-light" className="uppercase">{t('validatorManagement.details.explore')}</Typography>
                     <i className="bi-arrow-up-right text-primary"/>
                   </div>
                 </div>
               </a>
             </div>
-            <div className="w-full flex shadow py-2 px-4">
+            <div className="w-full flex border-style100 shadow py-2 px-4">
               <div className="space-y-2 mr-4">
-                <Typography type="text-caption2" isUpperCase isBold>Status</Typography>
+                <Typography type="text-caption2" isUpperCase isBold>{t('validatorManagement.details.status')}</Typography>
                 <div className="flex space-x-2">
                   <Typography type="text-caption2" isUpperCase color="text-dark300">{status}</Typography>
                   {status && <StatusIcon status={status}/>}
@@ -134,18 +134,18 @@ const ValidatorDetails = () => {
       </div>
       <ValidatorGraffitiInput/>
       <ValidatorDetailTable validator={validator}/>
-      <div className="w-full border-t space-y-4 opacity-60 p-4">
-        <Typography type="text-caption1">Validator Management</Typography>
+      <div className="w-full border-t-style100 space-y-4 opacity-60 p-4">
+        <Typography type="text-caption1">{t('validatorManagement.title')}</Typography>
         <div className="w-full flex flex-wrap lg:space-x-3">
-          <ValidatorCardAction className="w-32 @425:w-36 sm:w-96 mr-3 mb-2 lg:flex-1" icon="bi-arrow-down-circle" title="Deposit Funds"/>
-          <ValidatorCardAction className="w-32 @425:w-36 sm:w-96 mr-3 mb-2 lg:flex-1" icon="bi-arrow-down-circle" title="Deposit Funds"/>
-          <ValidatorCardAction className="w-32 @425:w-36 md:w-95 mr-3 mb-2 lg:flex-1" icon="bi-arrow-down-circle" title="Deposit Funds"/>
-          <ValidatorCardAction className="w-32 @425:w-36 md:w-95 mr-3 mb-2 lg:flex-1" icon="bi-arrow-down-circle" title="Deposit Funds"/>
-          <ValidatorCardAction className="w-32 @425:w-36 md:w-95 mr-3 mb-2 lg:flex-1" icon="bi-arrow-down-circle" title="Deposit Funds"/>
-          <ValidatorCardAction className="w-32 @425:w-36 md:w-95 mr-3 mb-2 lg:flex-1" icon="bi-arrow-down-circle" title="Deposit Funds"/>
+          <ValidatorCardAction icon="bi-arrow-down-circle" title={t('validatorManagement.actions.depositFunds')}/>
+          <ValidatorCardAction icon="bi-key-fill" title={t('validatorManagement.actions.backupKeys')}/>
+          <ValidatorCardAction icon="bi-power" title={t('validatorManagement.actions.stopValidator')} />
+          <ValidatorCardAction icon="bi-arrow-right-square" title={t('validatorManagement.actions.exportValidator')} />
+          <ValidatorCardAction icon="bi-arrow-right-square" title={t('validatorManagement.actions.exitValidator')} />
+          <ValidatorCardAction icon="bi-arrow-right-square" title={t('validatorManagement.actions.withdrawValidator')} />
         </div>
       </div>
-      <div className="p-3 border-t">
+      <div className="p-3 border-t-style100">
         <ViewDisclosures/>
       </div>
     </div>
