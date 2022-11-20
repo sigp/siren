294a5debc7ab1a8d45526cbb7fa8e315c319d8eb
Mavrik <mrricki.m.usmc@gmail.com>
2022-11-20 22:28:38 +0000

    feat: added electron and forge wrappers to app

---
+++ src/main.ts
@@ -0,0 +1,49 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires,@typescript-eslint/ban-ts-comment
+// @ts-ignore
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { app, BrowserWindow } = require('electron')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const url = require('url')
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      devTools: false,
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  })
+  mainWindow.maximize()
+
+  // and load the index.html of the app.
+  const startUrl =
+    process.env.WEB_URL ||
+    url.format({
+      pathname: path.join(__dirname, '../build/index.html'),
+      protocol: 'file',
+      slashes: true,
+    })
+  mainWindow.loadURL(startUrl)
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools()
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
