5f793e3def478f712296a78ae1927447e6f1135b
Mavrik <mrricki.m.usmc@gmail.com>
2022-11-22 19:08:17 +0000

    fix: add geth sync card image

--- src/components/SyncCard/SyncCard.tsx
+++ src/components/SyncCard/SyncCard.tsx
@@ -1,6 +1,7 @@
 import Typography from '../Typography/Typography'
 import ProgressCircle from '../ProgressCircle/ProgressCircle'
 import { FC } from 'react'
+import { ReactComponent as EthOutline } from '../../assets/images/eth-outline.svg'
 import { ReactComponent as NotAvailable } from '../../assets/images/notAvalilable.svg'
 import { ReactComponent as LightEth } from '../../assets/images/lightEth.svg'
 
@@ -33,9 +34,11 @@ const SyncCard: FC<SyncCardProps> = ({
       <div className='relative w-full flex items-center justify-center'>
         <ProgressCircle percent={progress} size='xl' id='geth' />
         <div className='absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2'>
-          {type ? (
+          {type === 'beacon' ? (
             <LightEth className='h-24 text-dark100' />
-          ) : (
+          ) : type === 'geth' ? (
+            <EthOutline className='h-24 text-dark100'/>
+            ) : (
             <NotAvailable className='h-16 text-dark100' />
           )}
         </div>
