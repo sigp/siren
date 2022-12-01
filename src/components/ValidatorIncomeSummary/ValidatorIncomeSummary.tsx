c24932fab20470ac3f0047abe934ffd48d2c369d
Mavrik <mrricki.m.usmc@gmail.com>
2022-12-01 14:55:21 +0000

    feat: added buttons to increment and decrement timeframe for income summary

--- src/components/ValidatorIncomeSummary/ValidatorIncomeSummary.tsx
+++ src/components/ValidatorIncomeSummary/ValidatorIncomeSummary.tsx
@@ -2,7 +2,7 @@ import Typography from '../Typography/Typography';
 import { useTranslation } from 'react-i18next';
 import useValidatorEarnings from '../../hooks/useValidatorEarnings';
 import { FC, useEffect, useState } from 'react';
-import { slotsInDay } from '../../constants/constants';
+import { EARNINGS_OPTIONS } from '../../constants/constants';
 import { ValidatorInfo } from '../../types/validator';
 
 export interface ValidatorIncomeSummaryProps {
@@ -12,6 +12,9 @@ export interface ValidatorIncomeSummaryProps {
 
 const ValidatorIncomeSummary:FC<ValidatorIncomeSummaryProps> = ({className, validators}) => {
   const {t} = useTranslation()
+  const [timeFrameIndex, setIndex] = useState(1);
+
+  const activeIncomeTimeFrame = EARNINGS_OPTIONS[timeFrameIndex]
 
   const { fetchHistory } = useValidatorEarnings(validators);
   const [income, setIncome] = useState('0')
@@ -24,15 +27,32 @@ const ValidatorIncomeSummary:FC<ValidatorIncomeSummaryProps> = ({className, vali
     }
   }
 
+  const incrementIndex = () => {
+    setIndex(prev => {
+      const nextIndex = prev + 1
+      return nextIndex <= EARNINGS_OPTIONS.length - 1 ? nextIndex : 0
+    })
+  }
+
+  const decrementIndex = () => {
+    setIndex(prev => prev > 0 ? prev - 1 : EARNINGS_OPTIONS.length - 1)
+  }
+
   useEffect(() => {
-    void fetchIncome(slotsInDay)
-  }, [])
+    void fetchIncome(activeIncomeTimeFrame.value)
+  }, [activeIncomeTimeFrame])
 
   return (
     <div className={className}>
-      <Typography type="text-caption2" isBold isUpperCase>{t('validatorManagement.summary.totalIncome')}</Typography>
+      <div className="w-full flex items-center justify-between">
+        <Typography type="text-caption2" isBold isUpperCase>{t('validatorManagement.summary.totalIncome')}</Typography>
+        <div className="flex space-x-2">
+          <i onClick={decrementIndex} className="bi-chevron-left cursor-pointer text-xs dark:text-dark300"/>
+          <i onClick={incrementIndex} className="bi-chevron-right cursor-pointer text-xs dark:text-dark300"/>
+        </div>
+      </div>
       <div className="flex items-center justify-between">
-        <Typography color="text-dark300" type="text-caption1">{t('validatorManagement.day')}</Typography>
+        <Typography color="text-dark300" isCapitalize type="text-caption1">{t(activeIncomeTimeFrame.title)}</Typography>
         <div className="flex items-center space-x-1">
           <i className={`text-sm ${Number(income) > 0 ? 'bi-chevron-up text-success' : 'bi-chevron-down text-error'}`}/>
           <Typography isBold type="text-caption1">{income} ETH</Typography>
