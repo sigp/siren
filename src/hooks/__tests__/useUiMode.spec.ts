51e8a93f7d322c506edc7da6a45cfc17a83d48da
Mavrik <mrricki.m.usmc@gmail.com>
2023-01-06 09:07:01 +0000

    feat: created unit test for useUiMode

---
+++ src/hooks/__tests__/useUiMode.spec.ts
@@ -0,0 +1,22 @@
+import useUiMode from '../useUiMode'
+import { renderHook } from '@testing-library/react-hooks'
+import { mockedRecoilState } from '../../../test.helpers'
+import { UiMode } from '../../constants/enums'
+
+const toggleUiMode = () => jest.fn()
+
+describe('useUiMode hook', () => {
+  it('should return default values', () => {
+    mockedRecoilState.mockReturnValue([undefined, toggleUiMode])
+    const { result } = renderHook(() => useUiMode())
+
+    expect(result.current.mode).toBe(undefined)
+    expect(JSON.stringify(result.current.toggleUiMode)).toStrictEqual(JSON.stringify(toggleUiMode))
+  })
+  it('should return correct values', () => {
+    mockedRecoilState.mockReturnValue([UiMode.DARK, toggleUiMode])
+    const { result } = renderHook(() => useUiMode())
+
+    expect(result.current.mode).toBe(UiMode.DARK)
+  })
+})
