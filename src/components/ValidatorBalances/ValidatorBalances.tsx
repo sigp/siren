8bf84b648a392ee2f2de13f2aea1c6f6c784c4db
Mavrik <mrricki.m.usmc@gmail.com>
2023-01-24 19:38:44 +0000

    feat: added legend modal to validator balance chart

--- src/components/ValidatorBalances/ValidatorBalances.tsx
+++ src/components/ValidatorBalances/ValidatorBalances.tsx
@@ -1,12 +1,13 @@
-import { BALANCE_COLORS } from '../../constants/constants'
-import getAverageValue from '../../utilities/getAverageValue'
 import StepChart from '../StepChart/StepChart'
 import Typography from '../Typography/Typography'
 import { useTranslation } from 'react-i18next'
 import useValidatorEpochBalance from '../../hooks/useValidatorEpochBalance'
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
+import Rodal from 'rodal'
 import Spinner from '../Spinner/Spinner'
 import LoadingDots from '../LoadingDots/LoadingDots'
+import useUiMode from '../../hooks/useUiMode'
+import CheckBox from '../CheckBox/CheckBox'
 
 export const ValidatorBalanceFallback = () => (
   <div className='flex-1 flex h-24 w-full justify-center items-center'>
@@ -16,29 +17,29 @@ export const ValidatorBalanceFallback = () => (
 
 const ValidatorBalances = () => {
   const { t } = useTranslation()
+  const { mode } = useUiMode()
+  const [hiddenValidators, setHiddenValidators] = useState<string[]>([])
+  const [isLegendModal, toggleModal] = useState(false)
   const { epochs, timestamps, isSufficientData } = useValidatorEpochBalance()
 
   const balanceData = useMemo(() => {
     return {
       labels: timestamps,
       datasets: epochs
-        .map(({ name, data }) => {
+        .filter(({ index }) => !hiddenValidators.includes(index))
+        .map(({ name, data, color }) => {
           return {
             label: name as string,
             data,
+            borderColor: color,
+            backgroundColor: color,
             fill: true,
             pointRadius: 0,
             stepped: 'after',
           }
-        })
-        .sort((a, b) => getAverageValue(a.data) - getAverageValue(b.data))
-        .map((data, index) => ({
-          ...data,
-          borderColor: BALANCE_COLORS[index],
-          backgroundColor: BALANCE_COLORS[index],
-        })),
+        }),
     }
-  }, [epochs, timestamps])
+  }, [epochs, timestamps, hiddenValidators])
 
   const stepSize = useMemo(() => {
     const data = balanceData.datasets
@@ -47,6 +48,17 @@ const ValidatorBalances = () => {
       .sort()
     return balanceData.datasets.length ? (data[data.length - 1] - data[0]) / 8 : 0
   }, [balanceData])
+
+  const onClose = () => toggleModal(false)
+  const toggleValidator = (index: string) => {
+    if (hiddenValidators.includes(index)) {
+      setHiddenValidators((prev) => prev.filter((prevIndex) => prevIndex !== index))
+    } else {
+      setHiddenValidators((prev) => [...prev, index])
+    }
+  }
+
+  const viewBalanceLegend = () => toggleModal(true)
 
   return (
     <div className='flex-1 flex h-full w-full'>
@@ -70,8 +82,48 @@ const ValidatorBalances = () => {
             {balanceData.datasets.length}
           </Typography>
         </div>
+        <Rodal
+          customStyles={{
+            backgroundColor: mode === 'DARK' ? '#1E1E1E' : 'white',
+            overflow: 'hidden',
+          }}
+          onClose={onClose}
+          visible={isLegendModal}
+        >
+          <div className='w-full h-full flex flex-col'>
+            <div className='w-full p-4 border-b-style500'>
+              <Typography isBold>Validator Legend</Typography>
+            </div>
+            <ul className='w-full flex-1 overflow-scroll'>
+              {epochs.map(({ name, index, color }, key) => (
+                <li
+                  onClick={() => toggleValidator(index)}
+                  className='cursor-pointer p-3 flex justify-between even:bg-dark10 dark:even:bg-dark750'
+                  key={key}
+                >
+                  <div className='flex items-center space-x-2'>
+                    <div className='w-3 h-3 border-black' style={{ backgroundColor: color }} />
+                    <Typography type='text-caption1'>
+                      {index}. <span className='font-bold ml-4'>{name}</span>
+                    </Typography>
+                  </div>
+                  <CheckBox
+                    onChange={() => toggleValidator(index)}
+                    checked={!hiddenValidators.includes(index)}
+                    id={index}
+                  />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Rodal>
         {isSufficientData ? (
-          <StepChart stepSize={stepSize} data={balanceData} />
+          <StepChart
+            onClick={viewBalanceLegend}
+            className='cursor-pointer'
+            stepSize={stepSize}
+            data={balanceData}
+          />
         ) : epochs.length ? (
           <div className='w-full h-full flex flex-col items-center justify-center'>
             <Typography color='text-primary' type='text-caption1' darkMode='dark:text-white'>
