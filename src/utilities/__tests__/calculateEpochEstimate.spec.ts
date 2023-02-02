ddbae0eefc82d2f8201c0bc3ace2b0a00a345208
Mavrik <mrricki.m.usmc@gmail.com>
2023-02-02 14:29:13 +0000

    feat: added unit test for epoch estimate util

---
+++ src/utilities/__tests__/calculateEpochEstimate.spec.ts
@@ -0,0 +1,31 @@
+import calculateEpochEstimate from '../calculateEpochEstimate'
+import { secondsInHour } from '../../constants/constants'
+import { formatUnits } from 'ethers/lib/utils'
+
+jest.mock('ethers/lib/utils', () => ({
+  formatUnits: jest.fn(),
+}))
+
+const mockedFormatUnits = formatUnits as jest.MockedFn<typeof formatUnits>
+
+describe('calculateEpochEstimate util', () => {
+  it('should return default value', () => {
+    expect(calculateEpochEstimate(secondsInHour)).toBe(0)
+  })
+  it('should return default value', () => {
+    expect(
+      calculateEpochEstimate(secondsInHour, {
+        2323: [32000000, 32000000],
+      }),
+    ).toBe(0)
+  })
+  it('should return correct values', () => {
+    mockedFormatUnits.mockReturnValue('3000')
+    expect(
+      calculateEpochEstimate(secondsInHour, {
+        2323: [32000000, 32000000],
+        2324: [32000000, 32000000],
+      }),
+    ).toBe(3000)
+  })
+})
