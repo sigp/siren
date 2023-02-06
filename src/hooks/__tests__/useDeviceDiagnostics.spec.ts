cf768af59a8c14b6b8abf3d380364ff6d5f1b9e2
Mavrik <mrricki.m.usmc@gmail.com>
2023-02-06 16:48:09 +0000

    feat: update unit test for device diagnostics

--- src/hooks/__tests__/useDeviceDiagnostics.spec.ts
+++ src/hooks/__tests__/useDeviceDiagnostics.spec.ts
@@ -3,13 +3,27 @@ import { renderHook } from '@testing-library/react-hooks'
 import formatGigBytes from '../../utilities/formatGigBytes'
 import { mockBeaconHealthResult } from '../../mocks/beaconHealthResult'
 import { mockedRecoilValue } from '../../../test.helpers'
+import { mockBeaconSyncResult } from '../../mocks/beaconSyncResults'
+import clearAllMocks = jest.clearAllMocks
+
+jest.mock('recoil', () => ({
+  useRecoilValue: jest.fn(),
+  useRecoilState: jest.fn(() => ['mock-value', jest.fn()]),
+  atom: jest.fn(),
+  selector: jest.fn(),
+}))
 
 jest.mock('../../utilities/formatGigBytes', () => jest.fn())
 
 const mockedFormatGigBytes = formatGigBytes as jest.MockedFn<typeof formatGigBytes>
 
 describe('useDeviceDiagnostics', () => {
+  beforeEach(() => {
+    clearAllMocks()
+  })
   it('should return default values', () => {
+    mockedRecoilValue.mockReturnValueOnce(undefined)
+    mockedRecoilValue.mockReturnValueOnce(mockBeaconSyncResult)
     const { result } = renderHook(() => useDeviceDiagnostics())
 
     expect(result.current).toStrictEqual({
@@ -32,6 +46,7 @@ describe('useDeviceDiagnostics', () => {
   })
   it('should return correct results', () => {
     mockedRecoilValue.mockReturnValueOnce(mockBeaconHealthResult)
+    mockedRecoilValue.mockReturnValueOnce(mockBeaconSyncResult)
     mockedFormatGigBytes.mockReturnValue(16.5)
     const { result } = renderHook(() => useDeviceDiagnostics())
 
@@ -52,5 +67,70 @@ describe('useDeviceDiagnostics', () => {
       healthCondition: 'POOR',
       overallHealthStatus: 'bg-error',
     })
+  })
+  it('should return bg-warning for disk space if syncing', () => {
+    mockedRecoilValue.mockReturnValueOnce(undefined)
+    mockedRecoilValue.mockReturnValueOnce({ ...mockBeaconSyncResult, isSyncing: true })
+    mockedFormatGigBytes.mockReturnValue(200)
+    const { result } = renderHook(() => useDeviceDiagnostics())
+
+    expect(result.current.diskStatus).toBe('bg-warning')
+  })
+  it('should return bg-success for disk space if syncing', () => {
+    mockedRecoilValue.mockReturnValueOnce(undefined)
+    mockedRecoilValue.mockReturnValueOnce(mockBeaconSyncResult)
+    mockedFormatGigBytes.mockReturnValue(200)
+    const { result } = renderHook(() => useDeviceDiagnostics())
+
+    expect(result.current.diskStatus).toBe('bg-success')
+  })
+  it('should return bg-success for ram status', () => {
+    mockedRecoilValue.mockReturnValueOnce(undefined)
+    mockedRecoilValue.mockReturnValueOnce(mockBeaconSyncResult)
+    mockedFormatGigBytes.mockReturnValueOnce(4)
+    mockedFormatGigBytes.mockReturnValueOnce(4)
+    mockedFormatGigBytes.mockReturnValueOnce(8)
+    mockedFormatGigBytes.mockReturnValueOnce(4)
+    const { result } = renderHook(() => useDeviceDiagnostics())
+
+    expect(result.current.ramStatus).toBe('bg-success')
+  })
+  it('should return bg-warning for ram status', () => {
+    mockedRecoilValue.mockReturnValueOnce(undefined)
+    mockedRecoilValue.mockReturnValueOnce(mockBeaconSyncResult)
+    mockedFormatGigBytes.mockReturnValueOnce(4)
+    mockedFormatGigBytes.mockReturnValueOnce(4)
+    mockedFormatGigBytes.mockReturnValueOnce(6)
+    mockedFormatGigBytes.mockReturnValueOnce(4)
+    const { result } = renderHook(() => useDeviceDiagnostics())
+
+    expect(result.current.ramStatus).toBe('bg-warning')
+  })
+  it('should return bg-warning for cpu status', () => {
+    mockedRecoilValue.mockReturnValueOnce({ ...mockBeaconHealthResult, sys_loadavg_5: 85 })
+    mockedRecoilValue.mockReturnValueOnce(mockBeaconSyncResult)
+    mockedFormatGigBytes.mockReturnValue(4)
+    const { result } = renderHook(() => useDeviceDiagnostics())
+
+    expect(result.current.cpuStatus).toBe('bg-warning')
+  })
+  it('should return bg-error for cpu status', () => {
+    mockedRecoilValue.mockReturnValueOnce({ ...mockBeaconHealthResult, sys_loadavg_5: 95 })
+    mockedRecoilValue.mockReturnValueOnce(mockBeaconSyncResult)
+    mockedFormatGigBytes.mockReturnValue(4)
+    const { result } = renderHook(() => useDeviceDiagnostics())
+
+    expect(result.current.cpuStatus).toBe('bg-error')
+  })
+  it('should return correct overall status', () => {
+    mockedRecoilValue.mockReturnValueOnce({ ...mockBeaconHealthResult, sys_loadavg_5: 85 })
+    mockedRecoilValue.mockReturnValueOnce(mockBeaconSyncResult)
+    mockedFormatGigBytes.mockReturnValueOnce(4)
+    mockedFormatGigBytes.mockReturnValueOnce(400)
+    mockedFormatGigBytes.mockReturnValueOnce(400)
+    mockedFormatGigBytes.mockReturnValueOnce(4)
+    const { result } = renderHook(() => useDeviceDiagnostics())
+
+    expect(result.current.overallHealthStatus).toStrictEqual('bg-warning')
   })
 })
