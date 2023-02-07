a345dab7b1ac2b694b7610c3e5004fcaaaa0b313
Mavrik <mrricki.m.usmc@gmail.com>
2023-02-07 12:03:07 +0000

    feat: added place param to tool tip and updated bg color

--- src/components/ToolTip/Tooltip.tsx
+++ src/components/ToolTip/Tooltip.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react'
-import { Tooltip as TooltipComponent } from 'react-tooltip'
+import { Tooltip as TooltipComponent, ITooltip } from 'react-tooltip'
 import useUiMode from '../../hooks/useUiMode'
 import { UiMode } from '../../constants/enums'
 
@@ -8,10 +8,11 @@ export interface TooltipProps {
   text: string
   className?: string
   maxWidth?: number
+  place?: ITooltip['place']
   children?: React.ReactNode
 }
 
-const Tooltip: FC<TooltipProps> = ({ id, text, children, className, maxWidth }) => {
+const Tooltip: FC<TooltipProps> = ({ id, text, children, className, maxWidth, place }) => {
   const { mode } = useUiMode()
 
   const isDarkMode = mode === UiMode.DARK
@@ -19,9 +20,10 @@ const Tooltip: FC<TooltipProps> = ({ id, text, children, className, maxWidth }) 
     <div id={id} className={className} data-tooltip-content={text}>
       {children}
       <TooltipComponent
+        place={place}
         style={{
           maxWidth,
-          backgroundColor: isDarkMode ? '#1E1E1E' : '#ffffff',
+          backgroundColor: isDarkMode ? '#1E1E1E' : '#F5F5F5',
           color: isDarkMode ? 'white' : 'black',
         }}
         anchorId={id}
