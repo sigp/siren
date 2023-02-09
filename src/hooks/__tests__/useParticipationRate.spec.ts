e0602ad348715919a0fcb6e87863982602a18494
Ricki Moore <mrricki.m.usmc@gmail.com>
2023-02-09 01:59:53 +0000

    Feat: participation rate (#101)

---
+++ src/hooks/__tests__/useParticipationRate.spec.ts
@@ -0,0 +1,136 @@
+import { renderHook } from '@testing-library/react-hooks'
+import useParticipationRate from '../useParticipationRate'
+import React from 'react'
+import { fetchValidatorInclusion } from '../../api/beacon'
+import { mockedRecoilValue, mockResponse } from '../../../test.helpers'
+import { mockBeaconSyncResult } from '../../mocks/beaconSyncResults'
+
+const mockParticipationResult = {
+  current_epoch_active_gwei: 13022634000000000,
+  current_epoch_target_attesting_gwei: 10322447000000000,
+  previous_epoch_active_gwei: 13022634000000000,
+  previous_epoch_head_attesting_gwei: 9225503000000000,
+  previous_epoch_target_attesting_gwei: 10676372000000000,
+}
+
+jest.mock('../../api/beacon', () => ({
+  fetchValidatorInclusion: jest.fn(),
+}))
+
+const mockedFetchValidatorInclusion = fetchValidatorInclusion as jest.MockedFn<
+  typeof fetchValidatorInclusion
+>
+const mockSetData = jest.fn()
+const mockSetError = jest.fn()
+
+jest.mock('recoil', () => ({
+  useRecoilValue: jest.fn(),
+  useRecoilState: jest.fn(() => ['mock-value', jest.fn()]),
+  atom: jest.fn(),
+  selector: jest.fn(),
+}))
+
+describe('useParticipationRate hook', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+  it('should call fetchValidatorInclusion', () => {
+    mockedRecoilValue.mockReturnValueOnce({ ...mockBeaconSyncResult, head_slot: 1234567 })
+    mockedRecoilValue.mockReturnValueOnce('mocked-url')
+
+    renderHook(() => useParticipationRate())
+
+    expect(mockedFetchValidatorInclusion).toBeCalled()
+  })
+  it('should return default values', () => {
+    const { result } = renderHook(() => useParticipationRate())
+    expect(result.current).toEqual({
+      isInsufficientData: false,
+      rate: undefined,
+      status: 'bg-error',
+    })
+  })
+  it('should return participation rate', () => {
+    jest.spyOn(React, 'useState').mockReturnValueOnce([false, jest.fn()])
+    jest.spyOn(React, 'useState').mockReturnValueOnce([mockParticipationResult, jest.fn()])
+
+    const { result } = renderHook(() => useParticipationRate())
+    expect(result.current).toEqual({
+      isInsufficientData: false,
+      rate: 82,
+      status: 'bg-warning',
+    })
+  })
+  it('should return error status', () => {
+    jest.spyOn(React, 'useState').mockReturnValueOnce([false, jest.fn()])
+    jest
+      .spyOn(React, 'useState')
+      .mockReturnValueOnce([
+        { ...mockParticipationResult, previous_epoch_target_attesting_gwei: 1000002000000000 },
+        jest.fn(),
+      ])
+
+    const { result } = renderHook(() => useParticipationRate())
+    expect(result.current).toEqual({
+      isInsufficientData: false,
+      rate: 8,
+      status: 'bg-error',
+    })
+  })
+  it('should return success status', () => {
+    jest.spyOn(React, 'useState').mockReturnValueOnce([false, jest.fn()])
+    jest
+      .spyOn(React, 'useState')
+      .mockReturnValueOnce([
+        { ...mockParticipationResult, previous_epoch_target_attesting_gwei: 12676372000000000 },
+        jest.fn(),
+      ])
+
+    const { result } = renderHook(() => useParticipationRate())
+    expect(result.current).toEqual({
+      isInsufficientData: false,
+      rate: 97,
+      status: 'bg-success',
+    })
+  })
+  it('should set mock data', () => {
+    mockedRecoilValue.mockReturnValueOnce({ ...mockBeaconSyncResult, head_slot: 1234567 })
+    mockedRecoilValue.mockReturnValueOnce('mocked-url')
+
+    jest.spyOn(React, 'useState').mockReturnValue([false, mockSetData])
+
+    mockedFetchValidatorInclusion.mockReturnValue(
+      Promise.resolve({
+        ...mockResponse,
+        data: {
+          data: mockParticipationResult,
+        },
+      }),
+    )
+
+    renderHook(() => useParticipationRate())
+
+    expect(mockSetData).toBeCalled()
+  })
+  it('should set isInsufficientData true', () => {
+    mockedRecoilValue.mockReturnValueOnce({ ...mockBeaconSyncResult, head_slot: 1234567 })
+    mockedRecoilValue.mockReturnValueOnce('mocked-url')
+
+    jest.spyOn(React, 'useState').mockReturnValueOnce([false, mockSetError])
+    jest.spyOn(React, 'useState').mockReturnValueOnce([false, mockSetData])
+
+    mockedFetchValidatorInclusion.mockReturnValue(
+      Promise.reject({
+        response: {
+          data: {
+            message: 'NOT_FOUND: beacon state',
+          },
+        },
+      }),
+    )
+
+    renderHook(() => useParticipationRate())
+
+    expect(mockSetError).toBeCalled()
+  })
+})
