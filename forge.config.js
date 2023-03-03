5e24d905439ba4e341630b4b7d2a331e56752d9e
Ricki Moore <mrricki.m.usmc@gmail.com>
2023-03-03 09:41:28 +0000

    Fix: prod build mac (#141)

--- forge.config.js
+++ forge.config.js
@@ -1,7 +1,20 @@
+// eslint-disable-next-line no-undef,@typescript-eslint/no-var-requires
+require('dotenv').config()
+
 // eslint-disable-next-line no-undef
 module.exports = {
   packagerConfig: {
     icon: 'src/assets/images/sigma', // no file extension required
+    osxSign: {},
+    osxNotarize: {
+      tool: 'notarytool',
+      // eslint-disable-next-line no-undef
+      appleId: process.env.APPLE_ID,
+      // eslint-disable-next-line no-undef
+      appleIdPassword: process.env.APPLE_PASSWORD,
+      // eslint-disable-next-line no-undef
+      teamId: process.env.APPLE_TEAM_ID,
+    },
   },
   rebuildConfig: {},
   makers: [
