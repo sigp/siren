37ce237d0425aef37ff2ac71120b05f9bfc98fa4
Mavrik <mrricki.m.usmc@gmail.com>
2023-03-10 11:26:43 +0000

    feat: trigger network modal if error

--- src/hooks/useBeaconHealthPolling.ts
+++ src/hooks/useBeaconHealthPolling.ts
@@ -1,5 +1,10 @@
 import { useRecoilValue, useSetRecoilState } from 'recoil'
-import { beaconHealthResult, beaconHealthSyncInterval, beaconNodeEndpoint } from '../recoil/atoms'
+import {
+  beaconHealthResult,
+  beaconHealthSyncInterval,
+  beaconNetworkError,
+  beaconNodeEndpoint,
+} from '../recoil/atoms'
 import formatHealthEndpoint from '../utilities/formatHealthEndpoint'
 import usePollApi from './usePollApi'
 import { useEffect } from 'react'
@@ -8,12 +13,16 @@ const useBeaconHealthPolling = (time = 6000) => {
   const beaconEndpoint = useRecoilValue(beaconNodeEndpoint)
   const url = formatHealthEndpoint(beaconEndpoint)
   const setHealth = useSetRecoilState(beaconHealthResult)
+  const setBeaconNetworkError = useSetRecoilState(beaconNetworkError)
+
+  const setNetworkError = () => setBeaconNetworkError(true)
 
   const { response } = usePollApi({
     time,
     isReady: !!url,
     intervalState: beaconHealthSyncInterval,
     url,
+    onMaxError: setNetworkError,
   })
 
   useEffect(() => {
