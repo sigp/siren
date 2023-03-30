6ac8b0e44ce537c341c02c2fb990b160b042984b
Age Manning <Age@AgeManning.com>
2023-03-30 17:13:18 +1100

    Temp CI commit (#131)
    Co-authored-by: Mavrik <mrricki.m.usmc@gmail.com>

--- forge.config.js
+++ forge.config.js
@@ -19,22 +19,8 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {
-        name: 'Siren',
-      },
-    },
-    {
       name: '@electron-forge/maker-zip',
       platforms: ['darwin', 'mas', 'win32', 'linux'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
     },
     {
       name: '@electron-forge/maker-dmg',
