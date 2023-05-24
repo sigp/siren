c49574e7e119ce5191d9bb67cea9294a3374137e
Mavrik <mrricki.m.usmc@gmail.com>
2023-05-24 18:22:24 +0200

    feat: expose start and stop of refresh interval and added manual refresh to sseProvider

--- src/components/SSELogProvider/SSELogProvider.tsx
+++ src/components/SSELogProvider/SSELogProvider.tsx
@@ -1,5 +1,5 @@
 import useTrackLogs, { defaultLogData, trackedLogData } from '../../hooks/useTrackLogs'
-import { FC, ReactElement, createContext, useState, useEffect } from 'react'
+import { FC, ReactElement, createContext, useState, useEffect, useCallback } from 'react'
 import { useRecoilValue } from 'recoil'
 import { selectBeaconUrl } from '../../recoil/selectors/selectBeaconUrl'
 import { selectValidatorUrl } from '../../recoil/selectors/selectValidatorUrl'
@@ -9,32 +9,74 @@ export interface SSELogWrapperProps {
   children: ReactElement | ReactElement[]
 }
 
-export const SSEContext = createContext<{ beaconLogs: trackedLogData; vcLogs: trackedLogData }>({
+export const SSEContext = createContext<{
+  beaconLogs: trackedLogData
+  vcLogs: trackedLogData
+  intervalId: NodeJS.Timer | undefined
+  clearRefreshInterval: () => void
+  startRefreshInterval: () => void
+  triggerRefresh: () => void
+}>({
   beaconLogs: defaultLogData,
   vcLogs: defaultLogData,
+  intervalId: undefined,
+  clearRefreshInterval: () => {},
+  startRefreshInterval: () => {},
+  triggerRefresh: () => {},
 })
 
 const SSELogProvider: FC<SSELogWrapperProps> = ({ children, trigger = 10000 }) => {
   const [, setTrigger] = useState(false)
+  const [intervalId, setIntervalId] = useState<NodeJS.Timer | undefined>(undefined)
   const beaconUrl = useRecoilValue(selectBeaconUrl)
   const validatorUrl = useRecoilValue(selectValidatorUrl)
   const beaconLogs = useTrackLogs(beaconUrl ? `${beaconUrl}/lighthouse/logs` : undefined)
   const validatorLogs = useTrackLogs(beaconUrl ? `${validatorUrl}/logs` : undefined)
 
-  useEffect(() => {
+  const clearRefreshInterval = useCallback(() => {
+    if (intervalId) {
+      clearInterval(intervalId)
+      setIntervalId(undefined)
+    }
+  }, [intervalId])
+
+  const triggerRefresh = () => {
+    beaconLogs.cleanLogs()
+    validatorLogs.cleanLogs()
+    setTrigger((prevTrigger) => !prevTrigger)
+  }
+
+  const startRefreshInterval = useCallback(() => {
     const interval = setInterval(() => {
-      setTrigger((prevTrigger) => !prevTrigger)
-      beaconLogs.cleanLogs()
-      validatorLogs.cleanLogs()
+      triggerRefresh()
     }, trigger)
 
+    setIntervalId(interval)
+
+    return interval
+  }, [beaconLogs.cleanLogs, validatorLogs.cleanLogs, trigger])
+
+  useEffect(() => {
+    startRefreshInterval()
+  }, [])
+
+  useEffect(() => {
     return () => {
-      clearInterval(interval)
+      clearRefreshInterval()
     }
-  }, [beaconLogs.cleanLogs, validatorLogs.cleanLogs])
+  }, [clearRefreshInterval])
 
   return (
-    <SSEContext.Provider value={{ beaconLogs: beaconLogs, vcLogs: validatorLogs }}>
+    <SSEContext.Provider
+      value={{
+        beaconLogs: beaconLogs,
+        vcLogs: validatorLogs,
+        intervalId,
+        startRefreshInterval,
+        clearRefreshInterval,
+        triggerRefresh,
+      }}
+    >
       {children}
     </SSEContext.Provider>
   )
