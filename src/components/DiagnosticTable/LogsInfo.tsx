d8ccffbd206c62b9936c5b50ca8dca0e59444e0b
Mavrik <mrricki.m.usmc@gmail.com>
2023-05-29 11:55:52 +0200

    feat: add link to logs on logs info component

--- src/components/DiagnosticTable/LogsInfo.tsx
+++ src/components/DiagnosticTable/LogsInfo.tsx
@@ -5,13 +5,19 @@ import { SSEContext } from '../SSELogProvider/SSELogProvider'
 import LogStats from '../LogStats/LogStats'
 import { LogCounts } from '../../types'
 import useMediaQuery from '../../hooks/useMediaQuery'
+import { useSetRecoilState } from 'recoil'
+import { dashView } from '../../recoil/atoms'
+import { ContentView } from '../../constants/enums'
 
 const LogsInfo = () => {
   const { t } = useTranslation()
   const { beaconLogs, vcLogs } = useContext(SSEContext)
+  const setDashView = useSetRecoilState(dashView)
   const isMobile = useMediaQuery('(max-width: 425px)')
 
   const size = isMobile ? 'health' : 'md'
+
+  const viewAllLogs = () => setDashView(ContentView.LOGS)
 
   const combinedLogCounts = useMemo<LogCounts>(() => {
     return {
@@ -28,9 +34,11 @@ const LogsInfo = () => {
         <Typography type='text-caption1' color='text-primary' darkMode='dark:text-white' isBold>
           {t('logInfo.logs')}
         </Typography>
-        <Typography type='text-tiny' className='uppercase' color='text-dark400'>
-          {t('viewAll')}
-        </Typography>
+        <div onClick={viewAllLogs} className='cursor-pointer'>
+          <Typography type='text-tiny' className='uppercase' color='text-dark400'>
+            {t('viewAll')}
+          </Typography>
+        </div>
       </div>
       <LogStats
         critToolTip={t('logs.tooltips.combinedCritical')}
