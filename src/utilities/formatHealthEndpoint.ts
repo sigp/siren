cac5578be536c62e84b5caef030705cad04138b5
Mavrik <mrricki.m.usmc@gmail.com>
2023-06-02 00:02:25 +0200

    fix: removed unused health endpoint format

--- src/utilities/formatHealthEndpoint.ts
+++
@@ -1,8 +0,0 @@
-import { Endpoint } from '../types'
-
-const formatHealthEndpoint = (endpoint?: Endpoint) => {
-  const { protocol, address, port } = endpoint || {}
-  return endpoint ? `${protocol}://${address}:${port}/lighthouse/ui/health` : undefined
-}
-
-export default formatHealthEndpoint
