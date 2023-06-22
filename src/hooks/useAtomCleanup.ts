6612c547df071b5eac7fa6cfafdfc4280e366357
Mavrik <mrricki.m.usmc@gmail.com>
2023-06-22 12:46:20 +0200

    fix: added resets to dashboard and onboard views

---
+++ src/hooks/useAtomCleanup.ts
@@ -0,0 +1,64 @@
+import { useSetRecoilState } from 'recoil'
+import {
+  activeDevice,
+  beaconHealthResult,
+  beaconNetworkError,
+  beaconSyncInfo,
+  beaconVersionData,
+  dashView,
+  deviceSettings,
+  sessionAuthErrorCount,
+  validatorCacheBalanceResult,
+  validatorHealthResult,
+  validatorMetrics,
+  validatorNetworkError,
+  validatorPeerCount,
+  validatorStateInfo,
+  validatorSyncInfo,
+  validatorVersionData,
+} from '../recoil/atoms'
+import { ContentView } from '../constants/enums'
+
+const useAtomCleanup = () => {
+  const setDashView = useSetRecoilState(dashView)
+  const setValMetrics = useSetRecoilState(validatorMetrics)
+  const setValCache = useSetRecoilState(validatorCacheBalanceResult)
+  const setPeerCount = useSetRecoilState(validatorPeerCount)
+  const setBeaconHealth = useSetRecoilState(beaconHealthResult)
+  const setValHealth = useSetRecoilState(validatorHealthResult)
+  const setBeaconVersion = useSetRecoilState(beaconVersionData)
+  const setValVersion = useSetRecoilState(validatorVersionData)
+  const setValStateInfo = useSetRecoilState(validatorStateInfo)
+  const setBeaconSyncInfo = useSetRecoilState(beaconSyncInfo)
+  const setValSyncInfo = useSetRecoilState(validatorSyncInfo)
+  const setActiveDevice = useSetRecoilState(activeDevice)
+  const setDeviceSettings = useSetRecoilState(deviceSettings)
+  const isBeaconNetworkError = useSetRecoilState(beaconNetworkError)
+  const isValidatorNetworkError = useSetRecoilState(validatorNetworkError)
+  const setSessionAuthErrorCount = useSetRecoilState(sessionAuthErrorCount)
+
+  const resetDashboardAtoms = () => {
+    setDashView(ContentView.MAIN)
+    setValMetrics(undefined as any)
+    setValCache(undefined)
+    setPeerCount(undefined)
+    setBeaconHealth(undefined)
+    setValHealth(undefined)
+    setBeaconVersion(undefined)
+    setValVersion(undefined)
+    setActiveDevice(undefined as any)
+    isBeaconNetworkError(false)
+    isValidatorNetworkError(false)
+    setSessionAuthErrorCount(0)
+    setDeviceSettings(undefined as any)
+    setValSyncInfo(undefined as any)
+    setBeaconSyncInfo(undefined as any)
+    setValStateInfo(undefined as any)
+  }
+
+  return {
+    resetDashboardAtoms,
+  }
+}
+
+export default useAtomCleanup
