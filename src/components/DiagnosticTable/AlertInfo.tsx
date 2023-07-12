cf40aa07544bb2b732d84cd183523bf9548f3c27
Mavrik <mrricki.m.usmc@gmail.com>
2023-07-12 15:10:46 +0200

    feat: updated alert info and card components

--- src/components/DiagnosticTable/AlertInfo.tsx
+++ src/components/DiagnosticTable/AlertInfo.tsx
@@ -2,13 +2,31 @@ import Typography from '../Typography/Typography'
 import AlertCard from '../AlertCard/AlertCard'
 import { useTranslation } from 'react-i18next'
 import useDiagnosticAlerts from '../../hooks/useDiagnosticAlerts'
+import useDivDimensions from '../../hooks/useDivDimensions'
+import { useEffect, useMemo } from 'react'
+import sortAlertMessagesBySeverity from '../../utilities/sortAlerts'
 
 const AlertInfo = () => {
   const { t } = useTranslation()
-  const { natAlert, peerCountAlert } = useDiagnosticAlerts()
+  const { alerts, dismissAlert, resetDismissed } = useDiagnosticAlerts()
+  const isFiller = alerts.length < 6
+  const { ref, dimensions } = useDivDimensions()
+
+  const sortedAlerts = useMemo(() => {
+    return sortAlertMessagesBySeverity(alerts)
+  }, [alerts])
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      resetDismissed()
+    }, 60000)
+
+    return () => clearInterval(intervalId)
+  }, [])
+
   return (
-    <div className='h-full w-full flex flex-col'>
-      <div className='w-full h-12 flex items-center justify-between px-4 md:border-l-0 border-style500'>
+    <div ref={ref} className='h-full w-full flex flex-col border-l-0 border-t-0 border-style500'>
+      <div className='w-full h-12 flex items-center justify-between px-4 md:border-l-0 border-r-0 border-style500'>
         <Typography type='text-caption1' color='text-primary' darkMode='dark:text-white' isBold>
           {t('alertInfo.alerts')}
         </Typography>
@@ -16,25 +34,35 @@ const AlertInfo = () => {
           {t('viewAll')}
         </Typography>
       </div>
-      {natAlert && (
-        <AlertCard
-          status={natAlert.severity}
-          count={3}
-          subText={natAlert.subText}
-          text={natAlert.message}
-        />
+      {dimensions && (
+        <div
+          style={{ maxHeight: `${dimensions.height - 48}px` }}
+          className='h-full w-full flex flex-col'
+        >
+          {sortedAlerts.length > 0 && (
+            <div className={`overflow-scroll scrollbar-hide ${!isFiller ? 'flex-1' : ''}`}>
+              {sortedAlerts.map((alert) => {
+                const { severity, subText, message, id } = alert
+                return (
+                  <AlertCard
+                    key={id}
+                    status={severity}
+                    count={3}
+                    onClick={() => dismissAlert(alert)}
+                    subText={subText}
+                    text={message}
+                  />
+                )
+              })}
+            </div>
+          )}
+          {isFiller && (
+            <div className='flex-1 flex items-center justify-center'>
+              <i className='bi bi-lightning-fill text-primary text-h3 opacity-20' />
+            </div>
+          )}
+        </div>
       )}
-      {peerCountAlert && (
-        <AlertCard
-          status={peerCountAlert.severity}
-          count={2}
-          subText={peerCountAlert.subText}
-          text={peerCountAlert.message}
-        />
-      )}
-      <div className='flex-1 md:border-l-0 border-t-0 border-style500 flex items-center justify-center'>
-        <i className='bi bi-lightning-fill text-primary text-h3 opacity-20' />
-      </div>
     </div>
   )
 }
