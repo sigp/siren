f1b9e519674b488fca19c623a7070ff44f33965a
Mavrik <mrricki.m.usmc@gmail.com>
2023-07-24 18:33:53 +0200

    feat: updated validator graffiti component

--- src/components/ValidatorGraffitiInput/ValidatorGraffitiInput.tsx
+++ src/components/ValidatorGraffitiInput/ValidatorGraffitiInput.tsx
@@ -1,15 +1,28 @@
 import Typography from '../Typography/Typography'
 import ValidatorActionIcon from '../ValidatorActionIcon/ValidatorActionIcon'
 import { useTranslation } from 'react-i18next'
-import { FC } from 'react'
-import DisabledTooltip from '../DisabledTooltip/DisabledTooltip'
+import { ChangeEvent, FC, useState } from 'react'
+import Input from '../Input/Input'
 
 export interface ValidatorGraffitiInputProps {
   value?: string
+  isLoading?: boolean
+  onSubmit: (value: string) => void
 }
 
-const ValidatorGraffitiInput: FC<ValidatorGraffitiInputProps> = ({ value }) => {
+const ValidatorGraffitiInput: FC<ValidatorGraffitiInputProps> = ({
+  value,
+  onSubmit,
+  isLoading,
+}) => {
   const { t } = useTranslation()
+
+  const [input, setInput] = useState('')
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => setInput(e.target.value)
+  const isDirty = input && value !== input
+  const submitGraffiti = () => onSubmit(input as string)
+
   return (
     <div className='w-full border-t flex'>
       <div className='border-r py-2 px-6 w-42'>
@@ -18,12 +31,20 @@ const ValidatorGraffitiInput: FC<ValidatorGraffitiInputProps> = ({ value }) => {
         </Typography>
       </div>
       <div className='flex-1 flex justify-between items-center px-8 py-3'>
-        <Typography type='text-caption1' color='text-primary'>
-          {value || t('graffitiGoesHere')}
-        </Typography>
-        <DisabledTooltip>
-          <ValidatorActionIcon icon='bi-pencil-square' color='text-primary' />
-        </DisabledTooltip>
+        <Input
+          value={input}
+          onChange={onChange}
+          inputStyle='noBorder'
+          className='text-caption1 placeholder:text-primary text-primary'
+          placeholder={value || t('graffitiGoesHere')}
+        />
+        <ValidatorActionIcon
+          onClick={submitGraffiti}
+          isLoading={isLoading}
+          isDisabled={!isDirty}
+          icon='bi-pencil-square'
+          color='text-primary'
+        />
       </div>
     </div>
   )
