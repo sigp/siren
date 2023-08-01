576fa5d09082a2e52b4ff59895fcc630d0a9c794
Mavrik <mrricki.m.usmc@gmail.com>
2023-08-01 14:02:48 +0200

    feat: updated bottom bar and added siren logo

--- src/components/FootBar/FootBar.tsx
+++ src/components/FootBar/FootBar.tsx
@@ -1,24 +1,15 @@
-// import Typography from '../Typography/Typography'
 import Button, { ButtonFace } from '../Button/Button'
 import HealthMetric, { HealthMetricFallback } from './HealthMetric'
 import { Suspense } from 'react'
 import { DiscordUrl, LighthouseBookUrl } from '../../constants/constants'
+import { ReactComponent as Siren } from '../../assets/images/siren.svg'
 
 const FootBar = () => {
   return (
-    <div className='w-full h-11 dark:bg-dark900 border border-l-0 dark:border-dark800 border-dark200 flex justify-end'>
-      {/* <div className='hidden md:flex space-x-8 items-center dark:text-white px-3 dark:bg-darkFull'> */}
-      {/*   <Typography */}
-      {/*     type='text-caption1' */}
-      {/*     family='font-roboto' */}
-      {/*     isBold */}
-      {/*     darkMode='dark:text-white' */}
-      {/*     className='uppercase' */}
-      {/*   > */}
-      {/*     Siren */}
-      {/*   </Typography> */}
-      {/*   <i className='bi bi-siren' /> */}
-      {/* </div> */}
+    <div className='w-full h-11 dark:bg-dark900 border border-l-0 dark:border-dark800 border-dark200 flex justify-between'>
+      <div className='hidden md:flex space-x-2 items-center dark:text-white px-3 dark:bg-darkFull'>
+        <Siren />
+      </div>
       <div className='flex items-center space-x-4 w-max'>
         <Suspense fallback={<HealthMetricFallback />}>
           <HealthMetric />
