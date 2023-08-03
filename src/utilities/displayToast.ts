7800565e6c267a29ecdb8f67af4014d0128e0b59
Mavrik <mrricki.m.usmc@gmail.com>
2023-08-03 22:16:31 +0200

    fix: use correct type for options and allow for false autoclose

--- src/utilities/displayToast.ts
+++ src/utilities/displayToast.ts
@@ -1,10 +1,11 @@
 import { toast } from 'react-toastify'
 import { ToastType } from '../types'
+import { ToastOptions } from 'react-toastify/dist/types'
 
-const displayToast = (message: string, type: ToastType, options?: any) => {
+const displayToast = (message: string, type: ToastType, options?: ToastOptions) => {
   const toastOptions = {
     position: options?.position || 'top-right',
-    autoClose: options?.autoClose || 5000,
+    autoClose: options?.autoClose ?? 5000,
     style: type === ToastType.SUCCESS ? { background: '#70D370' } : undefined,
     theme: options?.theme || 'colored',
     hideProgressBar: options?.hideProgressBar || true,
