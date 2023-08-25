70c5b1218da8889cf39b230c321c3456f2847a84
Mavrik <mrricki.m.usmc@gmail.com>
2023-08-25 15:03:18 +0200

    fix: prevent app crash when exchange api fails

--- src/recoil/selectors/selectEthExchangeRates.ts
+++ src/recoil/selectors/selectEthExchangeRates.ts
@@ -1,11 +1,25 @@
 import { selector } from 'recoil'
 import axios from 'axios'
 
-export const selectEthExchangeRates = selector({
+type Rates = {
+  [currency: string]: number
+}
+
+type EthExchangeRates = {
+  rates: Rates
+  currencies: string[]
+}
+
+export const selectEthExchangeRates = selector<EthExchangeRates>({
   key: 'EthExchangeRates',
   get: async () => {
-    const { data } = await axios.get('https://api.coinbase.com/v2/exchange-rates?currency=ETH')
-    const rates = data.data.rates
+    let rates = {}
+    try {
+      const { data } = await axios.get('https://api.coinbase.com/v2/exchange-rates?currency=ETH')
+      rates = data.data.rates
+    } catch (e) {
+      console.error('error accessing exchange api')
+    }
 
     return {
       rates,
