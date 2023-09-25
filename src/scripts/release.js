37f9292b92ee99d63149e8c71b700c62c2163d7f
Mavrik <mrricki.m.usmc@gmail.com>
2023-09-25 17:13:31 +0200

    fix: removed file change and use npm version update in package json

--- src/scripts/release.js
+++ src/scripts/release.js
@@ -1,33 +1,9 @@
 const { exec } = require('child_process')
-const fs = require('fs')
-const path = require('path')
-
-function executeCommand(command) {
-  return new Promise((resolve, reject) => {
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        console.warn(`Error executing command: ${command}\n${stderr}`)
-        reject(error)
-      } else {
-        console.log(`Success executing command: ${command}\n${stdout}`)
-        resolve(stdout)
-      }
-    })
-  })
-}
 
 function isValidSemanticVersion(version) {
   const semVerPattern =
     /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?$/
   return semVerPattern.test(version)
-}
-
-function writeVersionToFile(version) {
-  const filePath = path.resolve(__dirname, '../constants/version.ts')
-  const content = "export const sirenVersion = 'v" + version + "';\n"
-
-  fs.writeFileSync(filePath, content, 'utf-8')
-  console.log(`Version file updated to v${version}`)
 }
 
 function executeGitCommand(command) {
@@ -76,8 +52,8 @@ async function main() {
   await safeExecuteGitCommand('git checkout unstable')
   await safeExecuteGitCommand('git pull')
 
-  writeVersionToFile(versionId)
-  await safeExecuteGitCommand('git add ../constants/version.ts')
+  await safeExecuteGitCommand(`npm version ${versionId} --no-git-tag-version`)
+  await safeExecuteGitCommand('git add ../../package.json')
   await safeExecuteGitCommand(
     `git commit --allow-empty -m "siren version updated: ${versionId}" --no-verify`,
   )
@@ -91,7 +67,7 @@ async function main() {
   await safeExecuteGitCommand(`git push origin v${versionId}`)
 
   console.log(`Successfully tagged and initiated release ${versionId}`)
-  process.exit(1) // Exit the script on error
+  process.exit(1)
 }
 
 main()
