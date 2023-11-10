3720b223b417f1a304b2f49946477a7410ab5e8c
Mavrik <mrricki.m.usmc@gmail.com>
2023-11-10 11:51:58 +0100

    fix: upgraded storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,23 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app",
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/preset-create-react-app',
     'storybook-dark-mode/register',
-    'storybook-tailwind-dark-mode'
+    'storybook-tailwind-dark-mode',
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+
+  docs: {
+    autodocs: true,
   },
 }
