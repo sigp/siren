f7089996e55b3f38a14ca38f3353c0116af8cbb6
Mavrik <mrricki.m.usmc@gmail.com>
2024-03-04 13:13:04 +0100

    feat: added next and updated configs

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    });
+
+    return config;
+  }
+}
+
+module.exports = nextConfig
