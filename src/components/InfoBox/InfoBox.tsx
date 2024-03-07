cc9f613385f8cccd3f029dd66bf15bb4576a7731
Mavrik <mrricki.m.usmc@gmail.com>
2024-03-07 11:01:59 +0100

    fix: exported warning icon to component

--- src/components/InfoBox/InfoBox.tsx
+++ src/components/InfoBox/InfoBox.tsx
@@ -1,5 +1,6 @@
 import React, { FC, ReactNode } from 'react'
 import addClassString from '../../../utilities/addClassString'
+import AlertIcon from '../AlertIcon/AlertIcon';
 import Typography from '../Typography/Typography'
 
 export enum InfoBoxType {
@@ -23,21 +24,10 @@ const InfoBox: FC<InfoBoxProps> = ({ type, children, text }) => {
     'w-full flex flex-col md:flex-row space-y-4 md:space-y-0 items-center p-6 rounded',
     [isInfo && 'bg-dark500', isError && 'bg-lightError', isWarning && 'bg-warning200'],
   )
-  const warningIconBackgroundClasses = addClassString(
-    'rounded-full md:mr-14 flex-shrink-0 flex items-center justify-center h-12 w-12',
-    [isInfo && 'bg-dark400', isError && 'bg-lightError200', isWarning && 'bg-warning'],
-  )
-  const warningIconClasses = addClassString('text-2xl', [
-    isInfo && 'bi-hourglass-split text-dark600',
-    isError && 'bi-exclamation-triangle-fill text-error',
-    isWarning && 'bi-exclamation text-4xl text-warning900',
-  ])
 
   return (
     <div className={warningClasses}>
-      <div className={warningIconBackgroundClasses}>
-        <i className={warningIconClasses} />
-      </div>
+      <AlertIcon type={type.toLowerCase() as any}/>
       {children ? (
         children
       ) : text ? (
