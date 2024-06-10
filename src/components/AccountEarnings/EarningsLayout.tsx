b79d8c6a4a7c46739c50b66f44a703bb700da071
Mavrik <mrricki.m.usmc@gmail.com>
2024-06-10 10:52:09 +0200

    feat: upgraded earning wav anim

--- src/components/AccountEarnings/EarningsLayout.tsx
+++ src/components/AccountEarnings/EarningsLayout.tsx
@@ -1,6 +1,5 @@
-import Image from 'next/image'
 import { FC, ReactElement } from 'react'
-import Waves from '../../assets/images/waves.png'
+import AnimatedHeader from '../AnimatedHeader/AnimatedHeader';
 
 export interface EarningsLayoutProps {
   children: ReactElement | ReactElement[]
@@ -12,11 +11,7 @@ const EarningsLayout: FC<EarningsLayoutProps> = ({ children, className }) => {
     <div className={`${className || ''} w-full relative overflow-hidden`}>
       <div className='w-full h-full bg-primaryBright absolute left-0 top-0 blur-3xl origin-center -rotate-45 translate-x-36 scale-125' />
       <div className='z-20 w-full h-36 absolute left-0 bottom-0 bg-gradient-to-t dark:from-dark750 from-white to-transparent' />
-      <Image
-        className='z-10 w-full h-full absolute left-0 top-0 opacity-10'
-        alt='waves'
-        src={Waves}
-      />
+      <AnimatedHeader className="w-full h-full absolute left-0 top-0"/>
       {children}
     </div>
   )
