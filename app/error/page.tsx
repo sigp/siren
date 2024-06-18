3094243365523371b82587ca5d01a369dcd3718d
Mavrik <mrricki.m.usmc@gmail.com>
2024-06-19 01:45:13 +0200

    feat: added transkays for error page

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,21 +1,27 @@
 import '../../src/global.css';
+import { useTranslation } from 'react-i18next';
+import Lighthouse from '../../src/assets/images/lightHouse.svg'
 import Button, { ButtonFace } from '../../src/components/Button/Button';
 import TopographyCanvas from '../../src/components/Topography/Topography';
 import Typography from '../../src/components/Typography/Typography';
 
 export default async function Page() {
+  const {t} = useTranslation()
   return (
     <div className='relative w-screen h-screen flex items-center justify-center bg-gradient-to-r from-primary to-tertiary'>
       <div className="absolute top-1/2 left-1/2 -translate-x-1/4 -translate-y-1/2 w-fit rounded-full overflow-hidden">
         <TopographyCanvas color="#F0F0F0" animate height={600} width={600} name="error"/>
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <Lighthouse className="text-white w-[500px] h-[500px]"  />
+        </div>
       </div>
       <div className="relative space-y-12 z-50 w-96 -translate-x-96">
         <div>
-          <Typography isBold color="text-white" type="text-h2">Opps!</Typography>
-          <Typography color="text-white" type="text-subtitle3">Siren seems to have encountered an unexpected error. Please review your configuration and or node connections to continue to your dashboard.</Typography>
+          <Typography darkMode="text-white" isBold color="text-white" type="text-h2">{t('errorPage.title')}</Typography>
+          <Typography color="text-white" darkMode="text-white" type="text-subtitle3">{t('error.title')}</Typography>
         </div>
         <div>
-          <Button href="/" type={ButtonFace.SECONDARY}>Go Back</Button>
+          <Button href="/" type={ButtonFace.SECONDARY}>{t('errorPage.cta')}</Button>
         </div>
       </div>
     </div>
