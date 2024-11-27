28ae400f14eaf382cb63f0c6fd5b5ba862d0f3cc
Mavrik <mrricki.m.usmc@gmail.com>
2024-11-27 13:10:13 +0100

    fix: removed unused recoil atoms and added new for wallet modal

--- src/recoil/atoms.ts
+++ src/recoil/atoms.ts
@@ -1,5 +1,5 @@
 import { atom } from 'recoil'
-import { AppView, ContentView, OnboardView, UiMode } from '../constants/enums'
+import { AppView, OnboardView, UiMode } from '../constants/enums'
 import {
   ActiveDevice,
   AlertMessage,
@@ -7,9 +7,8 @@ import {
   ProposerDuty,
   ValAliases,
 } from '../types'
-import { BeaconNodeSpecResults, BeaconValidatorMetricResults } from '../types/beacon'
+import { BeaconNodeSpecResults } from '../types/beacon'
 import { HealthDiagnosticResult } from '../types/diagnostic'
-import { ValidatorCache } from '../types/validator'
 
 export const uiMode = atom<UiMode>({
   key: 'UiMode',
@@ -19,11 +18,6 @@ export const uiMode = atom<UiMode>({
 export const appView = atom<AppView | undefined>({
   key: 'AppView',
   default: undefined,
-})
-
-export const dashView = atom<ContentView>({
-  key: 'DashView',
-  default: ContentView.MAIN,
 })
 
 export const onBoardView = atom<OnboardView>({
@@ -41,23 +35,8 @@ export const beaconHealthResult = atom<HealthDiagnosticResult | undefined>({
   default: undefined,
 })
 
-export const validatorPeerCount = atom<number | undefined>({
-  key: 'validatorPeerCount',
-  default: undefined,
-})
-
 export const exchangeRates = atom<EthExchangeRates | undefined>({
   key: 'exchangeRates',
-  default: undefined,
-})
-
-export const activeCurrency = atom<string>({
-  key: 'activeCurrency',
-  default: undefined,
-})
-
-export const validatorCacheBalanceResult = atom<ValidatorCache | undefined>({
-  key: 'validatorCacheBalanceResult',
   default: undefined,
 })
 
@@ -74,11 +53,6 @@ export const validatorNetworkError = atom<boolean>({
 export const beaconNetworkError = atom<boolean>({
   key: 'beaconNetworkError',
   default: false,
-})
-
-export const validatorMetrics = atom<BeaconValidatorMetricResults[]>({
-  key: 'validatorMetrics',
-  default: undefined,
 })
 
 export const sessionAuthErrorCount = atom<number>({
@@ -134,4 +108,9 @@ export const isEditValidator = atom<boolean>({
 export const isValidatorDetail = atom<boolean>({
   key: 'isValidatorDetail',
   default: undefined
+})
+
+export const isWalletConnectModal = atom<boolean>({
+  key: 'isWalletConnectModal',
+  default: false
 })
