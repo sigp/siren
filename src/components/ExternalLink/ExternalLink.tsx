27923e28eea9f9bbf098ce97b82fa417a0d2282a
Mavrik <mrricki.m.usmc@gmail.com>
2024-11-28 19:07:50 +0100

    feat: added new helper components

---
+++ src/components/ExternalLink/ExternalLink.tsx
@@ -0,0 +1,22 @@
+import Link, {LinkProps} from 'next/link';
+import { FC } from 'react';
+import Typography, { TypographyProps } from '../Typography/Typography';
+
+export interface ExternalLinkProps extends LinkProps, Omit<TypographyProps, 'children'> {
+  text: string
+}
+
+const ExternalLink:FC<ExternalLinkProps> = ({href, text, color = 'text-dark400', type = 'text-caption1', ...props}) => {
+  return (
+    <div>
+      <Link href={href} target="_blank">
+        <div className="flex space-x-2 items-center">
+          <Typography color={color} type={type} className="underline" {...props}>{text}</Typography>
+          <i className="text-dark400 text-caption1 bi-box-arrow-up-right"/>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default ExternalLink;
