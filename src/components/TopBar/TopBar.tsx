9a7861f35c50bf1481ebbe1b4a03398909abd095
Mavrik <mrricki.m.usmc@gmail.com>
2024-11-28 19:10:14 +0100

    feat: add wallet and history components to topbar

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,29 +1,34 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import { FC } from 'react'
-import { useTranslation } from 'react-i18next'
-import { useSetRecoilState } from 'recoil'
-import LightHouseFullLogo from '../../assets/images/lightHouseFull.svg'
-import SlasherLogo from '../../assets/images/slasher.svg'
-import { isSideBarOpen } from '../../recoil/atoms'
-import { SyncData } from '../../types/beacon'
-import BeaconNetwork from '../BeaconNetwork/BeaconNetwork'
-import Button, { ButtonFace } from '../Button/Button'
-import Typography from '../Typography/Typography'
-import BeaconMetric from './BeaconMetric'
-import ValidatorMetric from './ValidatorMetric'
+import React, { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useSetRecoilState } from 'recoil';
+import LightHouseFullLogo from '../../assets/images/lightHouseFull.svg';
+import SlasherLogo from '../../assets/images/slasher.svg';
+import { isSideBarOpen } from '../../recoil/atoms';
+import { ActivityResponse } from '../../types';
+import { BeaconNodeSpecResults, SyncData } from '../../types/beacon';
+import ActivityHistory from '../ActivityHistory/ActivityHistory';
+import BeaconNetwork from '../BeaconNetwork/BeaconNetwork';
+import Button, { ButtonFace } from '../Button/Button';
+import Typography from '../Typography/Typography';
+import WalletProvider from '../Wallet/WalletProvider';
+import BeaconMetric from './BeaconMetric';
+import ValidatorMetric from './ValidatorMetric';
 
 export interface TopBarProps {
   syncData: SyncData
+  beaconSpec: BeaconNodeSpecResults
+  initActivityData: ActivityResponse
 }
 
-const TopBar: FC<TopBarProps> = ({ syncData }) => {
+const TopBar: FC<TopBarProps> = ({ syncData, beaconSpec, initActivityData }) => {
   const { t } = useTranslation()
+  const { DEPOSIT_NETWORK_ID } = beaconSpec
   const toggleSideBar = useSetRecoilState(isSideBarOpen)
   const { beaconSync, executionSync } = syncData
   const openSideBar = () => toggleSideBar(true)
 
   return (
-    <div className='w-full lg:h-14 @1540:h-18 border-b bg-white dark:bg-dark750 border-borderLight dark:border-dark800 flex justify-between'>
+    <div className='w-full z-[60] lg:h-14 @1540:h-18 border-b bg-white dark:bg-dark750 border-borderLight dark:border-dark800 flex justify-between'>
       <div className='flex h-full'>
         <div className='flex items-center md:w-42 border-r border-borderLight dark:border-borderDark px-4'>
           <div
@@ -56,21 +61,22 @@ const TopBar: FC<TopBarProps> = ({ syncData }) => {
         <BeaconNetwork />
       </div>
       <div className='h-full flex'>
-        <div className='h-full flex opacity-20'>
-          <ConnectButton/>
-          {/* <Wallet className='hidden lg:flex' borderStyle='border-l' /> */}
-          <Button
-            className='hidden md:block items-center border-l border-borderLight dark:border-borderDark'
-            type={ButtonFace.ICON}
-          >
-            <i className='bi bi-clock-history text-2xl text-dark300' />
-          </Button>
-          <Button
-            className='hidden md:block items-center border-l border-borderLight dark:border-borderDark'
-            type={ButtonFace.ICON}
-          >
-            <i className='bi bi-bell-fill text-2xl text-dark300' />
-          </Button>
+        <div className='h-full flex'>
+          <WalletProvider beaconSpec={beaconSpec} />
+          <div className="flex">
+            <ActivityHistory depositNetworkId={DEPOSIT_NETWORK_ID} initActivityData={initActivityData}/>
+            <div className="border-l-style">
+              <div className="opacity-30">
+                <Button
+                  isDisabled
+                  className='hidden md:block items-center dark:border-borderDark'
+                  type={ButtonFace.ICON}
+                >
+                  <i className='bi bi-bell-fill text-2xl text-dark300' />
+                </Button>
+              </div>
+            </div>
+          </div>
         </div>
         {/* <div className='h-full w-8 bg-gradient-to-b from-primary to-secondary flex items-center justify-center cursor-pointer'> */}
         {/*   <i className='bi bi-three-dots-vertical text-white text-3xl' /> */}
