5465663cabc9c1e9b3f06234d58c0aa89068b4e0
Mavrik <mavrik.dev@gmail.com>
2025-03-10 19:22:14 +0100

    fix: typing fix

--- utilities/getBeaconChaLink.ts
+++ utilities/getBeaconChaLink.ts
@@ -1,7 +1,7 @@
 import { NetworkId } from '../src/types'
 
 const getBeaconChaLink = (networkId: NetworkId, path: string) => {
-  return `https://${networkId === NetworkId.HOLESKY ? 'holesky.' : ''}beaconcha.in${path}`
+  return `https://${Number(networkId) === NetworkId.HOLESKY ? 'holesky.' : ''}beaconcha.in${path}`
 }
 
 export default getBeaconChaLink
