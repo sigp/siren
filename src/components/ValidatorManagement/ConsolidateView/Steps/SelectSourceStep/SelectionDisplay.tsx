1fb4865777962697d80407d1ff78fb9191fbb975
Mavrik <mavrik.dev@gmail.com>
2025-03-28 10:31:53 +0100

    feat: extract ui for effective balance into component

--- src/components/ValidatorManagement/ConsolidateView/Steps/SelectSourceStep/SelectionDisplay.tsx
+++ src/components/ValidatorManagement/ConsolidateView/Steps/SelectSourceStep/SelectionDisplay.tsx
@@ -1,10 +1,9 @@
 import clsx from 'clsx'
 import { FC, useMemo, memo } from 'react'
 import { useTranslation } from 'react-i18next'
-import formatEthAddress from '../../../../../../utilities/formatEthAddress'
 import ValidatorLogo from '../../../../../assets/images/validators.svg'
 import { ValidatorInfo } from '../../../../../types/validator'
-import Tooltip from '../../../../ToolTip/Tooltip'
+import EffectiveBalanceDisplay from '../../../../EffectiveBalanceDisplay/EffectiveBalanceDisplay'
 import Typography from '../../../../Typography/Typography'
 import SelectedChip from './SelectedChip'
 
@@ -24,10 +23,6 @@ const SelectionDisplay: FC<SelectionDisplayProps> = ({
   isOverMaxEb,
 }) => {
   const { t } = useTranslation()
-  const { name, pubKey, index } = targetValidator
-  const formattedPubKey = useMemo(() => formatEthAddress(pubKey, 7, 7), [pubKey])
-
-  const tooltipStyle = useMemo(() => ({ fontSize: '11px' }), [])
 
   const renderedChips = useMemo(
     () =>
@@ -38,6 +33,10 @@ const SelectionDisplay: FC<SelectionDisplayProps> = ({
   )
 
   const containerStyles = clsx('w-full border', isOverMaxEb ? 'border-error' : 'border-r-style')
+
+  const formattedTargetValidator = useMemo(() => {
+    return { ...targetValidator, effectiveBalance: totalEffectiveBalance }
+  }, [targetValidator, totalEffectiveBalance])
 
   return (
     <div className={containerStyles}>
@@ -50,48 +49,7 @@ const SelectionDisplay: FC<SelectionDisplayProps> = ({
         </div>
       </div>
       <div className='p-6'>
-        <div className='w-full flex justify-between items-center'>
-          <div className='flex sm:space-x-4'>
-            <div className='hidden sm:block h-16 w-16 rounded-full bg-gradient-to-r from-primary to-tertiary' />
-            <div className='space-y-1'>
-              <Typography>{name}</Typography>
-              <Tooltip
-                place='top-start'
-                style={tooltipStyle}
-                id={`tool-display-${pubKey}`}
-                text={pubKey}
-              >
-                <Typography type='text-caption' color='text-dark400' darkMode='dark:text-dark500'>
-                  {formattedPubKey}
-                </Typography>
-              </Tooltip>
-              <Typography type='text-caption' color='text-dark400' darkMode='dark:text-dark500'>
-                {index}
-              </Typography>
-            </div>
-          </div>
-          <div className='space-y-2'>
-            <Typography
-              className='break-keep text-right'
-              isBold
-              type='text-subtitle1'
-              color={isOverMaxEb ? 'text-error' : 'text-primary'}
-              darkMode={isOverMaxEb ? 'dark:text-error' : 'dark:text-primary'}
-            >
-              {`${totalEffectiveBalance} ETH`}
-            </Typography>
-            {isOverMaxEb && (
-              <Typography
-                type='text-tiny'
-                className='max-w-[150px] text-right'
-                color='text-error'
-                darkMode='dark:text-error'
-              >
-                {t('validatorManagement.consolidateView.overMaxEbErrorText')}
-              </Typography>
-            )}
-          </div>
-        </div>
+        <EffectiveBalanceDisplay validator={formattedTargetValidator} />
         {selectedSources.length > 0 && (
           <div className='py-4 flex lg:max-h-[150px] overflow-scroll flex-wrap border-t-style mt-6'>
             {renderedChips}
