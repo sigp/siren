7d3d1696cf8dce42b85a54e242f729f8fb9a3190
Mavrik <mavrik.dev@gmail.com>
2025-04-10 22:24:34 +0200

    feat: added utils for work count and mnemonic stats

---
+++ utilities/getMnemonicStats.ts
@@ -0,0 +1,17 @@
+import {TypographyColor} from "../src/components/Typography/Typography";
+
+const getMnemonicStats = (wordCount: number): {limit: number, color: TypographyColor, isValid: boolean} => {
+  const validMnemonicLengths = [12,15,18,21,24]
+
+  const limit = validMnemonicLengths.find(x => x >= wordCount) || validMnemonicLengths[validMnemonicLengths.length - 1]
+  const color = wordCount === limit ? 'text-success' : wordCount > limit ? 'text-error' : undefined as TypographyColor
+  const isValid = validMnemonicLengths.includes(wordCount)
+
+  return {
+    limit,
+    color,
+    isValid
+  }
+}
+
+export default getMnemonicStats
