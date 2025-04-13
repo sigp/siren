ad39d81ffe59197bcbfcd7658d6119a298d74010
Ricki Moore <mavrik.dev@gmail.com>
2025-04-13 10:47:05 +0200

    fix: block behind (#354)

--- utilities/__tests__/formatAtHeadSlotStatus.spec.ts
+++ utilities/__tests__/formatAtHeadSlotStatus.spec.ts
@@ -4,9 +4,9 @@ import { StatusColor } from '../../src/types'
 describe('formatAtHeadSlotStatus', () => {
   it('should return correct status', () => {
     expect(formatAtHeadSlotStatus(0)).toBe(StatusColor.SUCCESS)
-    expect(formatAtHeadSlotStatus(-1)).toBe(StatusColor.SUCCESS)
-    expect(formatAtHeadSlotStatus(-3)).toBe(StatusColor.WARNING)
-    expect(formatAtHeadSlotStatus(-5)).toBe(StatusColor.ERROR)
+    expect(formatAtHeadSlotStatus(1)).toBe(StatusColor.SUCCESS)
+    expect(formatAtHeadSlotStatus(3)).toBe(StatusColor.WARNING)
+    expect(formatAtHeadSlotStatus(5)).toBe(StatusColor.ERROR)
     expect(formatAtHeadSlotStatus()).toBe(StatusColor.DARK)
     expect(formatAtHeadSlotStatus(NaN)).toBe(StatusColor.DARK)
   })
--- utilities/formatAtHeadSlotStatus.ts
+++ utilities/formatAtHeadSlotStatus.ts
@@ -4,11 +4,11 @@ const formatAtHeadSlotStatus = (status?: number): StatusColor => {
   if (status === undefined) return StatusColor.DARK
 
   switch (true) {
-    case status >= -1:
+    case status <= 1:
       return StatusColor.SUCCESS
-    case status <= -2 && status > -5:
+    case status >= 2 && status < 5:
       return StatusColor.WARNING
-    case status <= -5:
+    case status >= 5:
       return StatusColor.ERROR
     default:
       return StatusColor.DARK
