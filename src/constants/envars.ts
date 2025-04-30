cb3987a2337e2dd1256e9334e55e9d7a4102be8d
Mavrik <mavrik.dev@gmail.com>
2025-04-30 23:25:12 +0200

    fix: fix default backend port

--- src/constants/envars.ts
+++ src/constants/envars.ts
@@ -1,3 +1,3 @@
 export const BACKEND_URL = process.env.BACKEND_URL || 'http://127.0.0.1:3001'
-export const BACKEND_PORT = process.env.BACKEND_PORT || 30001
+export const BACKEND_PORT = process.env.BACKEND_PORT || 3001
 export const PORT = process.env.PORT || 3000
