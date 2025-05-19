9047e05dbb6ac1ffbf0e0a5836e6eee1de7d79e2
Mavrik <mavrik.dev@gmail.com>
2025-05-19 12:53:27 +0200

    feat: add endpoints for log history and search

--- app/api/logs.ts
+++ app/api/logs.ts
@@ -2,6 +2,28 @@ import { BACKEND_URL } from '../../src/constants/envars'
 import { LogType } from '../../src/types'
 import fetchFromApi from '../../utilities/fetchFromApi'
 
+export interface fetchLogDataProps {
+  token: string
+  type?: LogType | undefined
+  limit?: string | undefined
+  offset?: string | undefined
+  level?: string | undefined
+}
+
+export interface searchLogDataProps {
+  token: string
+  type?: LogType | undefined
+  search?: string | undefined
+}
+
+export interface fetchPriorityProps {
+  token: string
+  type?: string
+  limit?: string
+  order?: string
+  since?: string
+}
+
 export const fetchLogMetrics = async (token: string) =>
   fetchFromApi(`${BACKEND_URL}/logs/metrics`, token)
 export const dismissLogAlert = async (token: string, index: string) =>
@@ -15,14 +37,6 @@ export const fetchMetrics = async (token: string, type?: LogType) => {
   return await fetchFromApi(`${BACKEND_URL}/logs/log-metrics?${params.toString()}`, token)
 }
 
-export interface fetchPriorityProps {
-  token: string
-  type?: string
-  limit?: string
-  order?: string
-  since?: string
-}
-
 export const fetchPriorityLogs = async (props: fetchPriorityProps) => {
   const { token, type, limit, order, since } = props
   const params = new URLSearchParams()
@@ -33,4 +47,24 @@ export const fetchPriorityLogs = async (props: fetchPriorityProps) => {
   if (since) params.append('since', since)
 
   return await fetchFromApi(`${BACKEND_URL}/logs/priority-logs?${params.toString()}`, token)
+}
+
+export const fetchLogData = async (props: fetchLogDataProps) => {
+  const { token, offset, limit, type, level } = props || {}
+  const params = new URLSearchParams()
+  if (type) params.append('type', type)
+  if (offset) params.append('offset', offset)
+  if (limit) params.append('limit', limit)
+  if (level) params.append('level', level)
+
+  return await fetchFromApi(`${BACKEND_URL}/logs/history?${params.toString()}`, token)
+}
+
+export const searchLogData = async (props: searchLogDataProps) => {
+  const { token, search, type } = props || {}
+  const params = new URLSearchParams()
+  if (type) params.append('type', type)
+  if (search) params.append('search', search)
+
+  return await fetchFromApi(`${BACKEND_URL}/logs/search?${params.toString()}`, token)
 }
