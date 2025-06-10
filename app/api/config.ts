b561775b25e132582c8c8bbcd15a7adf029c3f1c
Mavrik <mavrik.dev@gmail.com>
2025-06-10 17:55:56 +0200

    feat: add api routes for exclusion controller

--- app/api/config.ts
+++ app/api/config.ts
@@ -9,3 +9,5 @@ export const fetchValidatorVersion = async (token: string) =>
   await fetchFromApi(`${BACKEND_URL}/validator/version`, token)
 export const fetchGenesisData = async (token: string) =>
   await fetchFromApi(`${BACKEND_URL}/beacon/genesis`, token)
+export const fetchValidatorStatusExclusionList = async (token: string) =>
+  await fetchFromApi(`${BACKEND_URL}/exclude-status`, token)
