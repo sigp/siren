c7a95f44841f166a4a11d314841d5680a5e0267e
Yaroslav <eahome00@gmail.com>
2025-10-21 07:31:17 +0300

    Do not enable "secure" flag for cookies if SSL is disabled (#428)

--- src/constants/envars.ts
+++ src/constants/envars.ts
@@ -2,3 +2,4 @@ export const BACKEND_URL = process.env.BACKEND_URL || 'http://127.0.0.1:3001'
 export const BACKEND_PORT = process.env.BACKEND_PORT || 3001
 export const PORT = process.env.PORT || 3300
 export const NODE_ENV = process.env.NODE_ENV
+export const SSL_ENABLED = process.env.SSL_ENABLED === 'true'
